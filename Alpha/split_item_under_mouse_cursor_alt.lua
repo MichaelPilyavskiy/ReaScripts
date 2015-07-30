ccd6c7efb266b126c34531779e33933360d02f6a
MichaelPilyavskiy <m.pilyavskiy@gmail.com>
2015-07-30 20:20:31 +0300

    Create split_item_under_mouse_cursor_alt.lua

---
+++ Alpha/split_item_under_mouse_cursor_alt.lua
@@ -0,0 +1,9 @@
+-- if no items selected, nothing happend
+
+window, segment, details = reaper.BR_GetMouseCursorContext()
+if details == "item" then 
+  item = reaper.BR_GetMouseCursorContext_Item()
+  position = reaper.BR_GetMouseCursorContext_Position()
+  reaper.SplitMediaItem(item, position)
+end  
+reaper.UpdateArrange()
