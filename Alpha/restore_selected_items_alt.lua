2c789f616dff3b9feebe574dd9062a183f1ebfd7
MichaelPilyavskiy <m.pilyavskiy@gmail.com>
2015-08-02 22:37:41 +0300

    Update restore_selected_items_alt.lua

--- Alpha/restore_selected_items_alt.lua
+++ Alpha/restore_selected_items_alt.lua
@@ -1,20 +1,22 @@
-retval, count_stored_guids_s = reaper.GetExtState("Sel_items_guid", "count")
+count_stored_guids_s = reaper.GetExtState("Sel_items_guid", "count")
 count_stored_guids = tonumber(count_stored_guids_s)
 reaper.DeleteExtState("Sel_items_guid", "count", true)
 
 if count_stored_guids ~= nil  then
  if count_stored_guids > 0 then 
   for i = 1, count_stored_guids do
-    retval, stored_guid = reaper.GetExtState("Sel_items_guid", i)
+    stored_guid = reaper.GetExtState("Sel_items_guid", i)
     reaper.DeleteExtState("Sel_items_guid", i, true)
-    retval, stored_track_guid = reaper.GetExtState("Sel_items_tracks_guid", i)
+    stored_track_guid = reaper.GetExtState("Sel_items_tracks_guid", i)
     reaper.DeleteExtState("Sel_items_tracks_guid", i, true)
     item = reaper.BR_GetMediaItemByGUID(0, stored_guid)
-    track = reaper.GetMediaItem_Track(item)
-    track_guid = reaper.GetTrackGUID(track)
-    if track_guid == stored_track_guid then
-      reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1)
-    end  
+    if item ~= nil then
+      track = reaper.GetMediaItem_Track(item)
+      track_guid = reaper.GetTrackGUID(track)
+      if track_guid == stored_track_guid then
+        reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1)
+      end  
+    end 
   end
  end 
 end  
