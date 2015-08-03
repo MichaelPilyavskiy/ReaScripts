8679a27926f41525d56de96c1c68fd8a775b1e28
MichaelPilyavskiy <m.pilyavskiy@gmail.com>
2015-08-03 16:04:12 +0300

    Update restore_selected_items_alt.lua

--- Alpha/restore_selected_items_alt.lua
+++ Alpha/restore_selected_items_alt.lua
@@ -6,17 +6,20 @@ if count_stored_guids ~= nil  then
  if count_stored_guids > 0 then 
   for i = 1, count_stored_guids do
     stored_guid = reaper.GetExtState("Sel_items_guid", i)
-    reaper.DeleteExtState("Sel_items_guid", i, true)
+    
     stored_track_guid = reaper.GetExtState("Sel_items_tracks_guid", i)
-    reaper.DeleteExtState("Sel_items_tracks_guid", i, true)
-    item = reaper.BR_GetMediaItemByGUID(0, stored_guid)
-    if item ~= nil then
+    
+    item = reaper.BR_GetMediaItemByGUID(0, stored_guid)    
+    item_s = tostring(item)
+    if item ~= nil and item_s ~= "userdata: 0000000000000000" then
       track = reaper.GetMediaItem_Track(item)
       track_guid = reaper.GetTrackGUID(track)
       if track_guid == stored_track_guid then
         reaper.SetMediaItemInfo_Value(item, "B_UISEL", 1)
       end  
     end 
+    reaper.DeleteExtState("Sel_items_guid", i, true)
+    reaper.DeleteExtState("Sel_items_tracks_guid", i, true)
   end
  end 
 end  
