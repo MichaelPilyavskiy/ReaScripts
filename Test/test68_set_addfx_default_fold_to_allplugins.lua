7c3b6367a372f895668b16af0f804ba5ee425e3c
MichaelPilyavskiy <m.pilyavskiy@gmail.com>
2015-08-25 22:31:12 +0300

    Update test68_set_addfx_default_fold_to_allplugins.lua

--- Test/test68_set_addfx_default_fold_to_allplugins.lua
+++ Test/test68_set_addfx_default_fold_to_allplugins.lua
@@ -1,3 +1,6 @@
+-- Here I want to set FX Browser folder to "All plugins". 
+-- So everytime after opening it I will see common folder (not the last viewed)
+
 -- ok for unknown reason this returns err value:
   string = reaper.SNM_GetIntConfigVar("lastview", 222)
  -- reaper.ShowConsoleMsg(string)
@@ -6,7 +9,7 @@
   retval, string = reaper.BR_Win32_GetPrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini")
  -- reaper.ShowConsoleMsg(string)
  
- -- so, I try to write the value
+ -- so, I try to write the value 
   reaper.SNM_SetIntConfigVar("lastview", 0)
  -- reaper.BR_Win32_WritePrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini") 
  
