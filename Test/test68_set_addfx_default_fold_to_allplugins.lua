904e9d037e4bcf6362c63209dc7788779e9faa4c
MichaelPilyavskiy <m.pilyavskiy@gmail.com>
2015-08-25 21:47:00 +0300

    Update test68_set_addfx_default_fold_to_allplugins.lua

--- Test/test68_set_addfx_default_fold_to_allplugins.lua
+++ Test/test68_set_addfx_default_fold_to_allplugins.lua
@@ -1,11 +1,13 @@
 -- ok for unknown reason this returns err value:
- -- string = reaper.SNM_GetIntConfigVar("lastview", 222)
+  string = reaper.SNM_GetIntConfigVar("lastview", 222)
  -- reaper.ShowConsoleMsg(string)
  
  -- this returns right value
- -- retval, string = reaper.BR_Win32_GetPrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini")
+  retval, string = reaper.BR_Win32_GetPrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini")
  -- reaper.ShowConsoleMsg(string)
  
  -- so, I try to write the value
   reaper.SNM_SetIntConfigVar("lastview", 0)
- -- reaper.BR_Win32_WritePrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini")  
+ -- reaper.BR_Win32_WritePrivateProfileString("REAPER-fxadd", "lastview", "0", reaper.GetResourcePath().. "\\REAPER.ini") 
+ 
+ -- And nothing happened
