b8704515ac0b570574fe7943cafdd138986c6e68
Michael <m.pilyavskiy@gmail.com>
2015-09-11 08:13:54 +0300

    Delete mpl_Quantize_tool_set_swing.lua

--- Tools/mpl_Quantize_tool_set_swing.lua
+++
@@ -1,6 +0,0 @@
--- set strenght value for mpl Quantize tool
- 
-is_new_value,filename,sectionID,cmdID,mode,resolution,val = reaper.get_action_context()
-val_ret=val/127
-value = tostring(val_ret)
-reaper.SetExtState("mplQT_settings", "Swing", value, true)
