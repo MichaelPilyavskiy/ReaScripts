a87a5a4a6bcba3944a1d6d243ce58c4c90153279
Michael <m.pilyavskiy@gmail.com>
2015-09-11 08:12:28 +0300

    Create mpl_Quantize_Tool_set_strength.lua

---
+++ Tools/mpl_Quantize_Tool_set_strength.lua
@@ -0,0 +1,6 @@
+-- set strenght value for mpl Quantize tool
+ 
+is_new_value,filename,sectionID,cmdID,mode,resolution,val = reaper.get_action_context()
+val_ret=val/127
+value = tostring(val_ret)
+reaper.SetExtState("mplQT_settings", "Strenght", value, true)
