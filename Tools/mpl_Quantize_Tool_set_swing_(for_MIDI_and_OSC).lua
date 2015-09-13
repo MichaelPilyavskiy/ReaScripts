9c323a41ab2a3f735ea9c24463ff511e290bea2f
Michael <m.pilyavskiy@gmail.com>
2015-09-13 09:12:06 +0300

    Update mpl_Quantize_Tool_set_swing_(for_MIDI_and_OSC).lua

--- Tools/mpl_Quantize_Tool_set_swing_(for_MIDI_and_OSC).lua
+++ Tools/mpl_Quantize_Tool_set_swing_(for_MIDI_and_OSC).lua
@@ -3,4 +3,4 @@
 is_new_value,filename,sectionID,cmdID,mode,resolution,val = reaper.get_action_context()
 val_ret=val/resolution
 value = tostring(val_ret)
-reaper.SetExtState("mplQT_settings", "Swing", value, true)
+reaper.SetExtState("mplQT_settings", "Swing", value, false)
