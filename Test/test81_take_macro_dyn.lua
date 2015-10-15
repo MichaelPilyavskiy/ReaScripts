dc6b01c0512237e9f436375258c850ab857f2222
Michael <m.pilyavskiy@gmail.com>
2015-10-15 11:10:02 +0300

    Update and rename test81_gate_silence.lua to test81_take_macro_dyn.lua

--- Test/test81_take_macro_dyn.lua
+++ Test/test81_take_macro_dyn.lua
@@ -1,7 +1,17 @@
-window_time = 0.2 -- sec
-threshold = -80 -- db
-attack = 0.5 -- sec
-release = 0.4 -- sec
+  
+  --gate
+  window_time = 0.2 -- sec
+  threshold = -80 -- db
+  attack = 0.5 -- sec
+  release = 0.4 -- sec
+  
+  --press
+  window_time2 = 0.02 -- sec
+  threshold2 = -30 -- db
+  --attack2 = 0.02 -- sec
+  --release2 = 0.05 -- sec
+  ratio = 0.95
+
 
 function create_point (time,val)
   takeenv = reaper.GetTakeEnvelope(take, 0)
@@ -32,18 +42,20 @@ end
         if not reaper.TakeIsMIDI(take) then
           
           --get accesor samples values+rms
-          rms_val_t = {}
-          fft_val_t = {}
           audio_accessor = reaper.CreateTakeAudioAccessor(take)
             src = reaper.GetMediaItemTake_Source(take)
             src_num_ch = 1--reaper.GetMediaSourceNumChannels(src)
             src_rate = reaper.GetMediaSourceSampleRate(src)              
-            window_samples = math.floor(src_rate * window_time)
+            
+ --gate
                         
             --loop through windows
+            rms_val_t = {}
+            window_samples = math.floor(src_rate * window_time)
             for read_pos = 0, item_len, window_time do        
                 audio_accessor_buffer = reaper.new_array(window_samples)
                 reaper.GetAudioAccessorSamples(audio_accessor,src_rate,src_num_ch,read_pos,window_samples,audio_accessor_buffer)                
+                
                 -- read window rms
                   rms = 0
                   audio_accessor_buffer_t = audio_accessor_buffer.table(1, window_samples)
@@ -58,7 +70,7 @@ end
                 audio_accessor_buffer.clear()                                              
             end -- loop every window
             
-            -- add gate points
+           
             for i = 2, #rms_val_t-1 do
               rms_val_it = rms_val_t[i]
               prev_rms_val_it = rms_val_t[i-1]
@@ -68,7 +80,6 @@ end
               prev_rms_val_it_db = 20*math.log(prev_rms_val_it)
               next_rms_val_it_db = 20*math.log(next_rms_val_it)
               
-              -- gate opens
               if rms_val_it_db < threshold then
                 if prev_rms_val_it_db > threshold then
                   create_point(i*window_time,1)
@@ -77,10 +88,60 @@ end
                 if next_rms_val_it_db > threshold then
                   create_point(i*window_time-attack,0)
                   create_point(i*window_time,1)                  
-                end                       
+                end                                      
               end
-            end
+            end  
             
+            
+ -- compressor               
+              
+             rms_val_t2 = {} 
+            --loop through windows
+             window_samples2 = math.floor(src_rate * window_time2)
+             for read_pos = 0, item_len, window_time2 do        
+                 audio_accessor_buffer = reaper.new_array(window_samples2)
+                 reaper.GetAudioAccessorSamples
+                  (audio_accessor,src_rate,src_num_ch,read_pos,window_samples2,audio_accessor_buffer)                
+                 -- read window rms
+                   rms = 0
+                   audio_accessor_buffer_t = audio_accessor_buffer.table(1, window_samples2)
+                   for i = 1, window_samples2 do
+                     sample_value = audio_accessor_buffer_t[i]
+                     sample_value_abs = math.abs(sample_value)
+                     rms = rms + sample_value_abs
+                   end
+                   rms = rms / window_samples2
+                   table.insert(rms_val_t2, rms)                    
+                 audio_accessor_buffer_t = {}
+                 audio_accessor_buffer.clear()                                              
+             end -- loop every window 
+             
+                           
+            for i = 2, #rms_val_t2-1 do
+              rms_val_it = rms_val_t2[i]
+              prev_rms_val_it = rms_val_t2[i-1]
+              next_rms_val_it = rms_val_t2[i+1]
+              
+              rms_val_it_db = 20*math.log(rms_val_it)
+              prev_rms_val_it_db = 20*math.log(prev_rms_val_it)
+              next_rms_val_it_db = 20*math.log(next_rms_val_it)
+                                      
+              if rms_val_it_db > threshold2 then
+                create_point(i*window_time2,ratio)
+                if prev_rms_val_it_db < threshold2 then
+                  create_point((i-1)*window_time2,1)
+                end
+                if next_rms_val_it_db < threshold2 then
+                  create_point((i+1)*window_time2,1)
+                end  
+                if next_rms_val_it_db > threshold2 and
+                   prev_rms_val_it_db > threshold2 then
+                    delete_point (i*window_time2)
+                end
+                   
+              end
+   
+            end
                                 
           reaper.DestroyAudioAccessor(audio_accessor)     
         end -- if not midi 
