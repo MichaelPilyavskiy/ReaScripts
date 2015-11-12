4e3f79f83d0bd28f75d7f96ca125625ae254d722
Michael <m.pilyavskiy@gmail.com>
2015-11-12 09:29:30 +0300

    Update and rename mpl_Pan_all_tracks_with_LR_at_the_trackname_end_respectively.lua to mpl_Pan_selected_tracks_with_LR_at_the_trackname_end_respectively.lua

--- Routing/mpl_Pan_selected_tracks_with_LR_at_the_trackname_end_respectively.lua
+++ Routing/mpl_Pan_selected_tracks_with_LR_at_the_trackname_end_respectively.lua
@@ -1,9 +1,9 @@
-script_title = "Pan all tracks with LR at the trackname end respectively"
+script_title = "Pan selected tracks with LR at the trackname end respectively"
  reaper.Undo_BeginBlock()
 
-if reaper.CountTracks(0) ~= nil then
-  for i = 1, reaper.CountTracks(0) do
-    tr = reaper.GetTrack(0,i-1)
+if reaper.CountSelectedTracks(0) ~= nil then
+  for i = 1, reaper.CountSelectedTracks(0) do
+    tr = reaper.GetSelectedTrack(0,i-1)
     _, tr_name = reaper.GetSetMediaTrackInfo_String(tr, 'P_NAME', '', false)
     tr_name_last_sym = string.upper(string.sub(tr_name,-1))
     if tr_name_last_sym == 'L' then 
