8bb9e572d71f9d589c7fecf149e7005658dd85a2
Michael <m.pilyavskiy@gmail.com>
2016-01-06 09:37:48 +0300

    Create mpl_Mapping Panel - change map (MIDI CC and OSC only).lua

---
+++ Various/mpl_Mapping Panel - change map (MIDI CC and OSC only).lua
@@ -0,0 +1,13 @@
+--[[
+   * ReaScript Name: mpl_Mapping Panel - change map (MIDI CC and OSC only).lua
+   * Lua script for Cockos REAPER
+   * Author: Michael Pilyavskiy (mpl)
+   * Author URI: http://forum.cockos.com/member.php?u=70694
+   * Licence: GPL v3
+   * Version: 1.0
+  ]]
+  
+is_new_value,filename,sectionID,cmdID,mode,resolution,val = reaper.get_action_context()
+val_ret=val/resolution
+value = tostring(math.floor(val_ret*8)) -- should be taken from data.map_count
+reaper.SetExtState("MPL_PANEL_MAPPINGS", "MAP", value, false)
