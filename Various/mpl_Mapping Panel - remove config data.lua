6bbd9fd9acaebd5d4e64d65b712810bfc215fb2a
Michael <m.pilyavskiy@gmail.com>
2016-01-09 01:18:23 +0300

    Create mpl_Mapping Panel - remove config data.lua

---
+++ Various/mpl_Mapping Panel - remove config data.lua
@@ -0,0 +1,10 @@
+--[[
+   * ReaScript Name: Remove mpl Mapping Panel config from current project
+   * Lua script for Cockos REAPER
+   * Author: Michael Pilyavskiy (mpl)
+   * Author URI: http://forum.cockos.com/member.php?u=70694
+   * Licence: GPL v3
+   * Version: 1.0
+  ]]
+  
+  reaper.SetProjExtState(0, 'MPL_PANEL_MAPPINGS', 'MPL_PM_DATA','')
