2d5f2ecfb9ee96616942487148031c7eec5492bc
mpl <m.pilyavskiy@gmail.com>
2016-09-16 23:16:17 +0300

    Create mpl_Show all armed track envelopes, hide unarmed.lua

---
+++ Envelopes/mpl_Show all armed track envelopes, hide unarmed.lua
@@ -0,0 +1,36 @@
+-- @description Show all armed track envelopes, hide unarmed
+-- @version 1.0
+-- @author mpl
+-- @website http://forum.cockos.com/member.php?u=70694
+-- @changelog
+--    + init release
+
+difference = -10
+
+function main()
+  for tr = 1, reaper.CountTracks(0) do
+    local track = reaper.GetTrack(0,tr-1)
+    if track then 
+      for i = 1,  reaper.CountTrackEnvelopes( track ) do
+        local env = reaper.GetTrackEnvelope( track, i-1 )
+        local br_env = reaper.BR_EnvAlloc( env, false )
+        local active, visible, armed, inLane, laneHeight, defaultShape, _, _, _, _, faderScaling = reaper.BR_EnvGetProperties( br_env )
+        
+        if armed then visible = true else visible = false end
+        
+        reaper.BR_EnvSetProperties( br_env, 
+                                  active, 
+                                  visible, 
+                                  armed, 
+                                  inLane, 
+                                  laneHeight, 
+                                  defaultShape, 
+                                  faderScaling )
+        reaper.BR_EnvFree( br_env, true )
+      end
+    end
+  end
+  
+end
+    
+main()
