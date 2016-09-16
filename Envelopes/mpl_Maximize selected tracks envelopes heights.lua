e7936fdeadfd8c07a5b7f50c6f402fe7f7092a02
mpl <m.pilyavskiy@gmail.com>
2016-09-16 23:01:14 +0300

    Create mpl_Maximize selected tracks envelopes heights.lua

---
+++ Envelopes/mpl_Maximize selected tracks envelopes heights.lua
@@ -0,0 +1,32 @@
+-- @description Maximize selected tracks envelopes heights
+-- @version 1.0
+-- @author mpl
+-- @website http://forum.cockos.com/member.php?u=70694
+-- @changelog
+--    + init release
+
+
+function main()
+  for sel_tr = 1, reaper.CountSelectedTracks(0) do
+    local track = reaper.GetSelectedTrack(0,sel_tr-1)
+    if track then 
+      for i = 1,  reaper.CountTrackEnvelopes( track ) do
+        local env = reaper.GetTrackEnvelope( track, i-1 )
+        local br_env = reaper.BR_EnvAlloc( env, false )
+        local active, visible, armed, inLane, laneHeight, defaultShape, _, _, _, _, faderScaling = reaper.BR_EnvGetProperties( br_env )
+        reaper.BR_EnvSetProperties( br_env, 
+                                  active, 
+                                  visible, 
+                                  armed, 
+                                  inLane, 
+                                  1000,--laneHeight, 
+                                  defaultShape, 
+                                  faderScaling )
+        reaper.BR_EnvFree( br_env, true )
+      end
+    end
+  end
+  
+end
+
+main()
