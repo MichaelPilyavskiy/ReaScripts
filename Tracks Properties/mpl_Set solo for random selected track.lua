37abb63c3392a975e07e254e404461da65761cbf
mpl <m.pilyavskiy@gmail.com>
2016-11-20 22:31:41 +0300

    Create mpl_Set solo for random selected track.lua

---
+++ Tracks Properties/mpl_Set solo for random selected track.lua
@@ -0,0 +1,14 @@
+-- @description Set solo for random selected track
+-- @version 1.0
+-- @author mpl
+-- @changelog
+--    + init
+-- @website http://forum.cockos.com/member.php?u=70694
+
+cnt = reaper.CountSelectedTracks(0)
+set_rand = math.floor(  math.random() * cnt)
+for i = 0, cnt-1 do
+  tr = reaper.GetSelectedTrack(0,i)
+  if i == set_rand then reaper.CSurf_OnSoloChange( tr, 1 ) else reaper.CSurf_OnSoloChange( tr, 0) end
+end
+reaper.UpdateArrange()
