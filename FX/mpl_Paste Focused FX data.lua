ccb758fd1b7d0473b930c1007d30f0c645a914d1
mpl <m.pilyavskiy@gmail.com>
2017-01-11 15:57:14 +0300

    Create mpl_Paste Focused FX data.lua

---
+++ FX/mpl_Paste Focused FX data.lua
@@ -0,0 +1,57 @@
+-- @version 1.0
+-- @author mpl
+-- @changelog
+--   + init 
+-- @description Paste Focused FX data
+-- @website http://forum.cockos.com/member.php?u=70694
+
+function TrackFX_GetState(track, fx_id, buffer)
+    -- fx_id 1-based
+    local chunk,chunk_opened      
+    -- chain table
+      _, chunk = reaper.GetTrackStateChunk(track, '')
+      local chunk_t = {}
+      for line in chunk:gmatch("[^\n]+") do  table.insert(chunk_t, line)  end      
+    -- extract fx chunks limits
+      local fx_chunks_limits = {}
+      for i = 1, #chunk_t do
+        if chunk_t[i]:find('BYPASS') ~= nil then
+          chunk_opened = true
+          table.insert(fx_chunks_limits, {i})
+        end
+        if chunk_t[i]:find('WAK') ~= nil and chunk_opened then
+          chunk_opened = false
+          fx_chunks_limits[#fx_chunks_limits][2]=i
+        end            
+      end        
+       
+    local cnt_input_fx = reaper.TrackFX_GetCount( track ) 
+        
+    if fx_id -1 >= cnt_input_fx then return end
+    local FX_state = table.concat(chunk_t, '\n', 
+      fx_chunks_limits[fx_id][1]+1,
+      fx_chunks_limits[fx_id][2]) 
+    
+    local Clear_FX_state = FX_state:match('[^>]+.')
+    local Repl_FX_state = buffer:gsub('==FXDATA==',Clear_FX_state)
+    
+    local chunk_out = table.concat(chunk_t, '\n',1, fx_chunks_limits[fx_id][1]-1)
+    chunk_out = chunk_out..'\n'..Repl_FX_state..'\n'
+    chunk_out = chunk_out..table.concat(chunk_t, '\n',fx_chunks_limits[fx_id][2]+1)
+    --reaper.ShowConsoleMsg(chunk_out)
+    return chunk_out
+  end
+  
+  ----------------------------------------------------------------------------
+  buffer = reaper.GetExtState( 'MPL_Copy_FX_Data', 'buf') 
+  
+  _, tracknumberOut, _, fxnumberOut = reaper.GetFocusedFX()
+  if tracknumberOut >= 0 and fxnumberOut >= 0 then
+    track = reaper.CSurf_TrackFromID( tracknumberOut, false )
+  
+    chunk_out = TrackFX_GetState(track,fxnumberOut+1,buffer)
+    if chunk_out then 
+      reaper.SetTrackStateChunk( track, chunk_out, true )
+      reaper.UpdateArrange()
+    end
+  end
