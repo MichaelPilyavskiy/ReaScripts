a6021a65ea7707db975c827cb16c10bcc7bc9001
mpl <m.pilyavskiy@gmail.com>
2017-01-29 16:22:25 +0300

    Create mpl_Open directory in explorer, define path.lua

---
+++ Various/mpl_Open directory in explorer, define path.lua
@@ -0,0 +1,9 @@
+-- @version 1.0
+-- @author MPL
+-- @changelog
+--   + init, usage - for quick shortcuts to user defined folders
+-- @description Open directory in explorer, define path
+-- @website http://forum.cockos.com/member.php?u=70694
+
+path = 'C:\\'
+os.execute( 'Start '..path)
