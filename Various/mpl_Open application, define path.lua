02c9c5f42b7d7e270912a2173ec7c72aa0d79e1a
mpl <m.pilyavskiy@gmail.com>
2017-01-29 16:22:44 +0300

    Create mpl_Open application, define path.lua

---
+++ Various/mpl_Open application, define path.lua
@@ -0,0 +1,9 @@
+-- @version 1.0
+-- @author MPL
+-- @changelog
+--   + init, usage - for quick shortcuts to user defined applications
+-- @description Open application, define path
+-- @website http://forum.cockos.com/member.php?u=70694
+
+app = 'calc.exe'
+os.execute(app)
