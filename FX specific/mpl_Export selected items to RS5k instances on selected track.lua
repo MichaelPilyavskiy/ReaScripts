25aad25298c66f3fc757ebee387be1100f776e95
Michael <m.pilyavskiy@gmail.com>
2017-03-16 21:07:13 +0300

    slicedloop2MIDI

--- FX specific/mpl_Export selected items to RS5k instances on selected track.lua
+++ FX specific/mpl_Export selected items to RS5k instances on selected track.lua
@@ -1,24 +1,20 @@
--- @version 1.10
+-- @version 1.11
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @description Export selected items to RS5k instances on selected track
 -- @changelog
---    + delete items after export
---    # fix version check
+--    + create MIDI item if selected items placed on same track (MIDI from sliced loop)
 
   local script_title = 'Export selected items to RS5k instances on selected track'
-  -------------------------------------------------------------------------------
-  local track = reaper.GetSelectedTrack(0,0)
-  if not track then return end
   
   -------------------------------------------------------------------------------
   function F_SetFXName(track, fx, new_name)
-    local edited_line,edited_line_id
+    local edited_line,edited_line_id,segm
     -- get ref guid
       if not track or not tonumber(fx) then return end
       local FX_GUID = reaper.TrackFX_GetFXGUID( track, fx )
       if not FX_GUID then return else FX_GUID = FX_GUID:gsub('-',''):sub(2,-2) end
-      plug_type = reaper.TrackFX_GetIOSize( track, fx )
+      local plug_type = reaper.TrackFX_GetIOSize( track, fx )
     -- get chunk t
       local _, chunk = reaper.GetTrackStateChunk( track, '', false )
       local t = {} for line in chunk:gmatch("[^\r\n]+") do t[#t+1] = line end
@@ -37,7 +33,7 @@
       if not edited_line then return end
       local t1 = {}
       for word in edited_line:gmatch('[%S]+') do t1[#t1+1] = word end
-      t2 = {}
+      local t2 = {}
       for i = 1, #t1 do
         segm = t1[i]
         if not q then t2[#t2+1] = segm else t2[#t2] = t2[#t2]..' '..segm end
@@ -49,7 +45,7 @@
   
       local out_line = table.concat(t2,' ')
       t[edited_line_id] = '<'..out_line
-      out_chunk = table.concat(t,'\n')
+      local out_chunk = table.concat(t,'\n')
       --msg(out_chunk)
       reaper.SetTrackStateChunk( track, out_chunk, false )
       reaper.UpdateArrange()
@@ -57,7 +53,7 @@
   -------------------------------------------------------------------------------   
   function GlueSelectedItemsIndependently()
     -- store GUIDs
-      GUIDs = {}
+      local GUIDs = {}
       for it_id = 1, reaper.CountSelectedMediaItems(0) do
         local item =  reaper.GetSelectedMediaItem( 0, it_id-1 )
         local it_GUID = reaper.BR_GetMediaItemGUID( item )
@@ -65,7 +61,7 @@
       end
       
     -- glue items
-      new_GUIDs = {}
+      local new_GUIDs = {}
       for i = 1, #GUIDs do
         local item = reaper.BR_GetMediaItemByGUID( 0, GUIDs[i] )
         if item then 
@@ -83,10 +79,10 @@
         local item = reaper.BR_GetMediaItemByGUID( 0, new_GUIDs[i] )
         if item then reaper.SetMediaItemSelected(item, true) end
       end
-    
+    reaper.UpdateArrange() 
   end
   ------------------------------------------------------------------------------- 
-  function ExportSelItemsToRs5k(base_pitch)
+  function ExportSelItemsToRs5k(track, base_pitch)      
     for i = 1, reaper.CountSelectedMediaItems(0) do
       local item = reaper.GetSelectedMediaItem(0,i-1)
       local take = reaper.GetActiveTake(item)
@@ -121,25 +117,28 @@
     if not reaper.APIExists('TrackFX_SetNamedConfigParm')  then return end
     return true
   end
-  -------------------------------------------------------------------------------    
-  function main(track)
-    if not vrs_check() then return end
-    
-    -- get base pitch
-    local ret, base_pitch = reaper.GetUserInputs( script_title, 1, 'Set base pitch', 60 )
-    if not ret 
-      or not tonumber(base_pitch) 
-      or tonumber(base_pitch) < 0 
-      or tonumber(base_pitch) > 127 then
-      return true
-    end
-    base_pitch = math.floor(tonumber(base_pitch))
-    reaper.PreventUIRefresh( -1 )
-    GlueSelectedItemsIndependently()
-    ExportSelItemsToRs5k(base_pitch)
-    reaper.Main_OnCommand(40006,0)--Item: Remove items
-    reaper.PreventUIRefresh( 1 )
-    return true
+   
+  -------------------------------------------------------------------------------   
+  function FormMIDItake_data()
+    local MIDI = {}
+    -- check for same track/get items info
+      local item = reaper.GetSelectedMediaItem(0,0)
+      if not item then return end
+      MIDI.it_pos = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' )
+      MIDI.it_end_pos = MIDI.it_pos + 0.1
+      local proceed_MIDI = true
+      local it_tr0 = reaper.GetMediaItemTrack( item )
+      for i = 1, reaper.CountSelectedMediaItems(0) do
+        local item = reaper.GetSelectedMediaItem(0,i-1)
+        local it_pos = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' )
+        local it_len = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
+        MIDI[#MIDI+1] = {pos=it_pos, end_pos = it_pos+it_len}
+        MIDI.it_end_pos = it_pos + it_len
+        local it_tr = reaper.GetMediaItemTrack( item )
+        if it_tr ~= it_tr0 then proceed_MIDI = false break end
+      end
+      
+    return proceed_MIDI, MIDI
   end
   ------------------------------------------------------------------------------- 
     function F_extract_filename(orig_name)
@@ -148,9 +147,72 @@
     reduced_name = reduced_name:sub(0,-1-reduced_name:reverse():find('%.'))
     return reduced_name
   end
+
+  
+  -------------------------------------------------------------------------------    
+  function AddMIDI(track, MIDI,base_pitch)    
+    if not MIDI then return end
+      local new_it = reaper.CreateNewMIDIItemInProj( track, MIDI.it_pos, MIDI.it_end_pos )
+      local new_tk = reaper.GetActiveTake( new_it )
+      for i = 1, #MIDI do
+        local startppqpos =  reaper.MIDI_GetPPQPosFromProjTime( new_tk, MIDI[i].pos )
+        local endppqpos =  reaper.MIDI_GetPPQPosFromProjTime( new_tk, MIDI[i].end_pos )
+        local ret = reaper.MIDI_InsertNote( new_tk, 
+            false, --selected, 
+            false, --muted, 
+            startppqpos, 
+            endppqpos, 
+            0, 
+            base_pitch+i-1, 
+            100, 
+            true)--noSortInOptional )
+          --if ret then reaper.ShowConsoleMsg('done') end
+      end
+      reaper.MIDI_Sort( new_tk )
+      reaper.GetSetMediaItemTakeInfo_String( new_tk, 'P_NAME', 'sliced loop', 1 )
+      reaper.UpdateArrange()    
+  end
   
   -------------------------------------------------------------------------------  
+  function main(track)   
+    -- track check
+      local track = reaper.GetSelectedTrack(0,0)
+      if not track then return end
+      
+    -- item check
+      local item = reaper.GetSelectedMediaItem(0,0)
+      if not item then return true end        
+    
+    -- get base pitch
+      local ret, base_pitch = reaper.GetUserInputs( script_title, 1, 'Set base pitch', 60 )
+      if not ret 
+        or not tonumber(base_pitch) 
+        or tonumber(base_pitch) < 0 
+        or tonumber(base_pitch) > 127 then
+        return 
+      end
+      base_pitch = math.floor(tonumber(base_pitch))
+    
+    -- glue items
+      GlueSelectedItemsIndependently()
+    
+    -- get info for new midi take
+      local proceed_MIDI, MIDI = FormMIDItake_data()
+      
+    -- export to RS5k
+      ExportSelItemsToRs5k(track, base_pitch)
+      reaper.Main_OnCommand(40006,0)--Item: Remove items
+    
+    -- add MIDI
+      if proceed_MIDI then AddMIDI(track, MIDI,base_pitch) end 
+      
+  end
+  
+    -------------------------------------------------------------------------------    
+  if not vrs_check() then reaper.MB('Script works with REAPER 5.40 and upper. Script also need SWS extension.','Error',0) end
   reaper.Undo_BeginBlock()
-  ret = main(track)
-  if not ret then reaper.MB('Script works with REAPER 5.40 and upper. Script also need SWS extension.','Error',0) end
+  --  reaper.PreventUIRefresh( -1 )  
+  main()  
+  -- reaper.PreventUIRefresh( 1 )
+   
   reaper.Undo_EndBlock(script_title, 1)
