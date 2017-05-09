c03d9d2036357bf07fa6f6c96cf45d624b95d3f3
Michael <m.pilyavskiy@gmail.com>
2017-05-09 14:41:59 +0300

    fix header

--- FX/mpl_Apply FX without sends, offline FX.lua
+++ FX/mpl_Apply FX without sends, offline FX.lua
@@ -1,9 +1,9 @@
-  -- @version 1.0
-  -- @author MPL
-  -- @website http://forum.cockos.com/member.php?u=70694
-  -- @description Apply FX without sends, offline FX
-  -- @changelog
-  --    # prevent overlap with last item or itself
+-- @version 1.0
+-- @author MPL
+-- @website http://forum.cockos.com/member.php?u=70694
+-- @description Apply FX without sends, offline FX
+-- @changelog
+--    # prevent overlap with last item or itself
   
   function ApplyFX()
     item = reaper.GetSelectedMediaItem(0,0) 
