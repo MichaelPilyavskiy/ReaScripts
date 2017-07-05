af9ca5da02450bda524ff71c98ebc6c9b16be4d7
Michael <m.pilyavskiy@gmail.com>
2017-07-05 23:38:57 +0300

    del bps offl fx

--- FX/mpl_Delete bypassed and offline fx from selected tracks.lua
+++ FX/mpl_Delete bypassed and offline fx from selected tracks.lua
@@ -1,68 +1,36 @@
--- @version 1.1
+-- @version 1.2
 -- @author MPL
 -- @changelog
---   # prevent deleting bypassed fx with bypass envelope
+--   # recoded for FX deleting via SWS API and smarter offline and bypass checking
 -- @description Delete bypassed fx from selected tracks
 -- @website http://forum.cockos.com/member.php?u=70694
   
-  function main()
-    local chunk, chunk_opened--,is_bypassed,is_offline
-    local counttracks = reaper.CountSelectedTracks(0)
-    if counttracks ~= nil then
-      for tr_id = 1, counttracks do
-        local track = reaper.GetSelectedTrack(0,tr_id-1)
-        if track ~= nil then
-          _, chunk = reaper.GetTrackStateChunk(track, '')
-          
-          local chunk_t = {}
-          -- chain table
-            for line in chunk:gmatch("[^\n]+") do  table.insert(chunk_t, line)  end
-            
-          -- extract fx chunks limits
-            fx_chunks = {}
-            local fx_ch_op
-            for i = 1, #chunk_t do
-              if chunk_t[i]:find('<FXCHAIN') then fx_ch_op = true end
-              if chunk_t[i]:find('<FXCHAIN_REC') then fx_ch_op = false end
-              if fx_ch_op then
-                if chunk_t[i]:find('BYPASS') ~= nil then
-                  chunk_opened = true
-                  fx_chunks[#fx_chunks+1] = {str='',lim1 = i}
-                end
-                if chunk_opened then fx_chunks[#fx_chunks].str = fx_chunks[#fx_chunks].str..'\n'..chunk_t[i] end
-                if chunk_t[i]:find('WAK') ~= nil and chunk_opened then 
-                  chunk_opened = false 
-                  fx_chunks[#fx_chunks].lim2 = i
-                end
-              end
-            end
-            
-          -- add/edit chunks
-            if #fx_chunks > 0 then
-              -- loop chunks
-                out_chunk = table.concat(chunk_t, '\n', 1, fx_chunks[1].lim1-1)
-                
-                for fx = 1, #fx_chunks do
-                  local fx_chunk = table.concat(chunk_t, '\n', fx_chunks[fx].lim1, fx_chunks[fx].lim2)
-                  is_bypassed = tonumber(chunk_t[fx_chunks[fx].lim1 ]:sub(8,8))
-                  bypass_env = fx_chunk:match('<PARMENV '..'[%d]+'..':bypass')
-                  if is_bypassed ~= 1 or bypass_env then 
-                    out_chunk = out_chunk..'\n'..table.concat(chunk_t, '\n', fx_chunks[fx].lim1, fx_chunks[fx].lim2)
-                  end
-                  --is_offline =  chunk_t[fx_chunks_limits[i][1] ]:sub(10,10)
-                end
-                
-                out_chunk = out_chunk..'\n'..table.concat(chunk_t, '\n', fx_chunks[#fx_chunks].lim2+1)
-                --reaper.ShowConsoleMsg("")
-                --reaper.ShowConsoleMsg(out_chunk)
-                reaper.SetTrackStateChunk(track, out_chunk)
-            end
-            
-        end
+  function RemoveBypassOfflineFX(track)
+    if not track then return end
+    local _, chunk = reaper.GetTrackStateChunk(track, '')
+    local t = {}
+    for line in chunk:gmatch('[^\r\n]+') do  table.insert(t, line)  end
+    local fx = {}
+    local fxGUID
+    for i =  #t, 1, -1 do
+      if t[i]:match('FXID') then ch_search = true fxGUID = t[i]:match("%{.+%}") end
+      if ch_search and t[i]:match('BYPASS') then
+        ch_search = nil
+        if fxGUID then fx[fxGUID] = tonumber(t[i]:match('%d %d %d'):gsub(' ', ''),2) end
       end
+    end    
+    for fx_id = reaper.TrackFX_GetCount( track ), 1, -1 do
+      local GUID = reaper.TrackFX_GetFXGUID( track, fx_id-1 )
+      msg(GUID)
+      if fx[GUID] and fx[GUID] > 0 then reaper.SNM_MoveOrRemoveTrackFX( track, fx_id-1, 0 ) end
     end
+    
   end
-  
+  --------------------------------------------------------------------
   reaper.Undo_BeginBlock()
-  main()
+  for i =1, reaper.CountSelectedTracks(0) do 
+    local tr = reaper.GetSelectedTrack(0,i-1)
+    if tr then RemoveBypassOfflineFX(tr) end
+  end
+  RemoveBypassOfflineFX()
   reaper.Undo_EndBlock('Delete bypassed fx from selected tracks', 0)
