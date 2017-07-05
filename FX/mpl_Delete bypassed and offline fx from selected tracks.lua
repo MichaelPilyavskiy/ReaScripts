e7feb9a75f91fedcff4ad8c4062a5521022ef8dc
Michael <m.pilyavskiy@gmail.com>
2017-07-05 23:41:34 +0300

    cng vrs

--- FX/mpl_Delete bypassed and offline fx from selected tracks.lua
+++ FX/mpl_Delete bypassed and offline fx from selected tracks.lua
@@ -1,4 +1,4 @@
--- @version 1.2
+-- @version 1.11
 -- @author MPL
 -- @changelog
 --   # recoded for FX deleting via SWS API and smarter offline and bypass checking
@@ -21,7 +21,6 @@
     end    
     for fx_id = reaper.TrackFX_GetCount( track ), 1, -1 do
       local GUID = reaper.TrackFX_GetFXGUID( track, fx_id-1 )
-      msg(GUID)
       if fx[GUID] and fx[GUID] > 0 then reaper.SNM_MoveOrRemoveTrackFX( track, fx_id-1, 0 ) end
     end
     
