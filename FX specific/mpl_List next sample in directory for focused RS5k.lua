516b4f2f58b161ee2283c1e6a070ee8d0fb4ec44
Michael <m.pilyavskiy@gmail.com>
2017-07-09 12:20:00 +0300

    list rs5k

---
+++ FX specific/mpl_List next sample in directory for focused RS5k.lua
@@ -0,0 +1,51 @@
+-- @version 1.0
+-- @author MPL
+-- @website http://forum.cockos.com/member.php?u=70694
+-- @description List next sample in directory for focused RS5k
+-- @changelog
+--    + init
+
+
+function main()
+    local ret, tracknumberOut, _, fxnumberOut = reaper.GetFocusedFX()
+    local track = reaper.CSurf_TrackFromID( tracknumberOut, false )
+    if not track then return end
+    ret, fn = reaper.TrackFX_GetNamedConfigParm(track, fxnumberOut, "FILE0")
+    if not ret then return end
+    fn = fn:gsub('\\', '/')
+    
+    path = fn:reverse():match('[%/]+.*'):reverse():sub(0,-2)
+    cur_file =     fn:reverse():match('.-[%/]'):reverse():sub(2)
+    -- get files list
+      local files = {}
+      local i = 0
+      repeat
+      local file = reaper.EnumerateFiles( path, i )
+      if file then
+        files[#files+1] = file
+      end
+      i = i+1
+      until file == nil
+      
+    -- search file list
+      local trig_file
+      if #files < 2 then return end
+      for i = 2, #files do
+        if files[i-1] == cur_file then 
+          trig_file = path..'/'..files[i] 
+          break 
+         elseif i == #files then trig_file = path..'/'..files[1] 
+        end
+        
+      end
+      
+      if trig_file then 
+        reaper.TrackFX_SetNamedConfigParm(track, fxnumberOut, "FILE0", trig_file)
+        reaper.TrackFX_SetNamedConfigParm(track, fxnumberOut, "DONE", "")
+      end
+  end
+
+
+  reaper.Undo_BeginBlock()
+  main(track)
+  reaper.Undo_EndBlock('List next sample in directory for focused RS5k', 1)
