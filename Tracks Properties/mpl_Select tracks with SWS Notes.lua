c4f113c28f4db5b695f0ef131bc52b19a853db2e
mpl <m.pilyavskiy@gmail.com>
2017-07-10 16:54:32 +0300

    Update mpl_Select tracks with SWS Notes.lua

--- Tracks Properties/mpl_Select tracks with SWS Notes.lua
+++ Tracks Properties/mpl_Select tracks with SWS Notes.lua
@@ -1,21 +1,19 @@
--- @version 1.0
--- @author MPL
+-- @version 1.1
+-- @author mpl
 -- @changelog
---   + init
+--   #proper match GUID
 -- @description Select tracks with SWS Notes
 -- @website http://forum.cockos.com/member.php?u=70694
-  
-  function msg(s) reaper.ShowConsoleMsg("") reaper.ShowConsoleMsg(s) end
-
 
   function main()
-    _, projfn = reaper.EnumProjects( -1, '' )
-    file = io.open(projfn, 'r')
+    local ext_line_C
+    local _, projfn = reaper.EnumProjects( -1, '' )
+    local file = io.open(projfn, 'r')
     if file then
       ext_line_C = ''
-      content = file:read("*all")
-      for ext_line in content:gmatch('S&M_TRACKNOTES.->') do 
-        ext_line_C = ext_line_C..'\n'..ext_line:sub(16)
+      local content = file:read("*all")      
+      for ext_line in content:gmatch('S&M_TRACKNOTES.-[\n]') do 
+        ext_line_C = ext_line_C..' '..ext_line:sub(17,-3):gsub('-', '')
       end
       file:close()
     end
@@ -24,7 +22,7 @@
     local cnt_tracks = reaper.CountTracks(0)
     for i = 1, cnt_tracks do
       local track = reaper.GetTrack(0,i-1)
-      local tr_guid =  reaper.GetTrackGUID( track )
+      local tr_guid =  reaper.GetTrackGUID( track ):sub(2,-2):gsub('-', '')
       if ext_line_C:find(tr_guid) then  
         reaper.SetTrackSelected( track, true )
        else
