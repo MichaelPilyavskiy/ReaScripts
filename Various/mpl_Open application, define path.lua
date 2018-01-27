720dd322e25b6d383a245a3a4b7ec88c14625fca
Michael <m.pilyavskiy@gmail.com>
2018-01-27 07:42:47 +0300

    force  background tag to defered scripts, remove obsolete

--- Various/mpl_Open application, define path.lua
+++
@@ -1,9 +0,0 @@
--- @version 1.0
--- @author MPL
--- @changelog
---   + init, usage - for quick shortcuts to user defined applications
--- @description Open application, define path
--- @website http://forum.cockos.com/member.php?u=70694
-
-app = 'calc.exe'
-os.execute(app)
--- Various/mpl_Open directory in explorer, define path.lua
+++
@@ -1,9 +0,0 @@
--- @version 1.0
--- @author MPL
--- @changelog
---   + init, usage - for quick shortcuts to user defined folders
--- @description Open directory in explorer, define path
--- @website http://forum.cockos.com/member.php?u=70694
-
-path = 'C:\\'
-os.execute( 'Start '..path)
