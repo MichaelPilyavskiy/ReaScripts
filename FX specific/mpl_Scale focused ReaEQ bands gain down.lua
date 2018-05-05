d2dc7da0fd561ee687a2c586a13793148400b81a
Michael <m.pilyavskiy@gmail.com>
2018-05-05 10:31:16 +0300

    reaeqscale

---
+++ FX specific/mpl_Scale focused ReaEQ bands gain down.lua
@@ -0,0 +1,33 @@
+-- @description Scale focused ReaEQ bands gain down
+-- @version 1.0
+-- @author MPL
+-- @website http://forum.cockos.com/showthread.php?t=188335
+  
+  ratio = 0.9
+  
+  
+  -- NOT reaper NOT gfx
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  function msg(s) if s then ShowConsoleMsg(s..'\n') end end
+  -----------------------------------------------------------------------------
+  function MPL_ScaleReaEqBand(ratio)
+    local  retval, tracknumber, itemnumber, fx = GetFocusedFX()
+    if not (retval ==1 and fx >= 0) then return end
+    local tr = CSurf_TrackFromID( tracknumber, false )
+    local isReaEQ = TrackFX_GetEQParam( tr, fx, 0 )
+    if not isReaEQ then return end
+    local num_params = TrackFX_GetNumParams( tr, fx )
+    ClearConsole()
+    for param = 1, num_params - 2, 3 do
+      local gain = TrackFX_GetParamNormalized( tr, fx, param  )
+      local out_gain = gain
+      if gain >0.5 then
+        out_gain = gain - (gain - 0.5)*(1-ratio)
+       elseif gain < 0.5 then 
+        out_gain = gain + (0.5-gain)*(1-ratio)
+      end
+      TrackFX_SetParamNormalized( tr, fx, param, math.max(0,math.min(1, out_gain ) ))
+    end
+  end
+  ----------------------------------------------------------------------------- 
+  MPL_ScaleReaEqBand(ratio)
