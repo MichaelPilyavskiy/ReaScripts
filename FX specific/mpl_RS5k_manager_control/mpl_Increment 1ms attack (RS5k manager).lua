08aa2cd227c43d7b1d689ee216372afe57d4618c
Michael <m.pilyavskiy@gmail.com>
2018-07-15 08:58:39 +0300

    rs5k ctrl

---
+++ FX specific/mpl_RS5k_manager_control/mpl_Increment 1ms attack (RS5k manager).lua
@@ -0,0 +1,23 @@
+-- @description Increment 1ms attack (RS5k manager)
+-- @version 1.0
+-- @author MPL
+-- @website https://forum.cockos.com/showthread.php?t=207971
+-- @provides
+--    mpl_RS5k_manager_control_functions.lua
+-- @changelog
+--    + init
+
+
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  
+  local info = debug.getinfo(1,'S');
+  script_path = info.source:match([[^@?(.*[\/])[^\/]-$]]):match('(.*)\\')
+  dofile(script_path .. "/mpl_RS5k_manager_control_functions.lua")  
+  ----------------------------  
+  function main()
+    incr = 1
+    param = 9    
+    SetGlobalParam(_, param, incr )
+  end
+  ----------------------------
+  defer(main)
