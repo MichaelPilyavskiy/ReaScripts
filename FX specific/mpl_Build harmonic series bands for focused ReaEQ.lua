20e7cebeea73b7154ad259083d442d0d639383f7
Michael <m.pilyavskiy@gmail.com>
2018-10-05 00:00:37 +0300

    0510

--- FX specific/mpl_Build harmonic series bands for focused ReaEQ.lua
+++ FX specific/mpl_Build harmonic series bands for focused ReaEQ.lua
@@ -1,11 +1,10 @@
 -- @description Build harmonic series bands for focused ReaEQ
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    + link bandwidth after building
---    + bands gain reduce factor
---    + remember settings
+--    # smaller init Q
+--    + option to link Gain [p=2042020]
   
   
   
@@ -18,7 +17,7 @@
      return math.max(min,  math.min(val, max) ) 
    end
   -----------------------------------------------------------------------------
-  function MPL_BuildEQReduce(freq, gain, slope, linkBW)
+  function MPL_BuildEQReduce(freq, gain, slope, linkBW, linkgain)
     if not (freq and gain and slope) then return end
     local  retval, tracknumber, itemnumber, fx = GetFocusedFX()
     if not (retval ==1 and fx >= 0) then return end
@@ -26,18 +25,21 @@
     local isReaEQ = TrackFX_GetEQParam( tr, fx, 0 )
     if not isReaEQ then return end
     local num_params = TrackFX_GetNumParams( tr, fx )
-    local build_str = ''
+    local linkBW_str = ''
+    local linkgain_str = ''
     local bands_cnt = math.floor((num_params - 2)/3)
     for param = 1, num_params - 2, 3 do
       local band_id = math.floor(param/3)
       TrackFX_SetNamedConfigParm( tr, fx, 'BANDTYPE'..band_id, 2 )
       TrackFX_SetParamNormalized( tr, fx, param, lim(gain + band_id * slope, 0, 0.5)) -- -2.0dB gain
-      local startQdenom = 20
+      local startQdenom = 40
       TrackFX_SetParamNormalized( tr, fx, param+1, lim(1/(startQdenom* (band_id+1)), 0.01, 1)) -- 0.5 Q
       local ConvertedF = F2ReaEQVal(freq * (band_id+1) )
       TrackFX_SetParamNormalized( tr, fx, param-1, ConvertedF) -- freq
       if param > 1 then 
-        build_str = build_str..'\n'..
+      
+        if linkBW then 
+          linkBW_str = linkBW_str..'\n'..
         -- Q
 [[      <PROGRAMENV ]]..(param+1)..[[ 0
         PARAMBASE 0
@@ -49,11 +51,29 @@
         MODWND 0
       >      
 ]]        
+        end
+        
+        if linkgain then
+          linkBW_str = linkBW_str..'\n'..
+        -- Q
+[[      <PROGRAMENV ]]..(param)..[[ 0
+        PARAMBASE 0
+        LFO 0
+        LFOWT 1 1
+        AUDIOCTL 0
+        AUDIOCTLWT 1 1
+        PLINK 1 0:0 1 0
+        MODWND 0
+      >      
+]]   
+      end
       end
     end
-    if linkBW then 
-      local out_chunk = LinkBands(tr, fx, build_str)
-      SetTrackStateChunk( tr, out_chunk, true )
+    
+    local out_chunk
+    if linkBW or linkgain then 
+      local out_chunk = Link(tr, fx, linkBW_str) -- link bandQ
+      SetTrackStateChunk( tr, out_chunk, true ) 
     end
   end
   ---------------------------------------------------
@@ -61,8 +81,10 @@
      if str then  return str:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", function(c) return "%" .. c end) end
   end
   -----------------------------------------------------------------------------
-  function LinkBands(tr, fx, build_str)
-    local retval, chunk = GetTrackStateChunk( tr, '', false )
+  function Link(tr, fx, build_str, chunk) local chunk
+    if not chunk then 
+      retval, chunk = GetTrackStateChunk( tr, '', false )
+    end
     local fxGUID = TrackFX_GetFXGUID( tr, fx )
     --local build_str = ''
     local chunk = chunk:gsub(literalize(fxGUID)..'.-WAK', fxGUID..build_str..'WAK')
@@ -95,19 +117,20 @@
     return x
   end
   ----------------------------------------------------------------------------- 
-  local def_params = '50,-150,0.01,Y'
+  local def_params = '50,-150,0.01,Y,N'
   local extstate = GetExtState('MPL_BUILDHARMSERREAEQ', 'params')
   if extstate ~= '' then def_params = extstate end
-  local retval, str = GetUserInputs('Build harmonic bands', 4, 'Base frequency(Hz) (def=50),Gain Reduction(dB) (def=-150),Gain reduce (def=0.01),Link banwidth (Y/N)', def_params)
+  local retval, str = GetUserInputs('Build harmonic bands', 5, 'Base frequency(Hz) (def=50),Gain Reduction(dB) (def=-150),Gain reduce (def=0.01),Link banwidth (Y/N),Link gain (Y/N)', def_params)
   if retval then 
-    local t, freq, gain, slope, linkbands = {}, _, 0, 0.01, true
+    local t, freq, gain, slope, linkbands, linkgain = {}, _, 0, 0.01, true, false
     for val in str:gmatch('[^,]+') do  t[#t+1] = val end
     if t[1] then freq = t[1] else return end
     if t[2] then gain = ParseDbVol(t[2]) end
     if t[3] then slope = t[3] end
     if t[4] then linkbands = t[4]:lower() == 'y' end
+    if t[5] then linkgain = t[5]:lower() == 'y' end
     if freq and gain and slope then 
       SetExtState( 'MPL_BUILDHARMSERREAEQ', 'params', table.concat(t, ','), true )
     end
-    MPL_BuildEQReduce(freq, lim(gain, 0, 0.5), slope, linkbands)
+    MPL_BuildEQReduce(freq, lim(gain, 0, 0.5), slope, linkbands, linkgain)
   end
