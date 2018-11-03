63054040b0098e71437dfce39ceabe3423122111
Michael <m.pilyavskiy@gmail.com>
2018-11-03 03:00:30 +0300

    311-2

---
+++ Various/mpl_QuantizeTool_presets/mpl_QuantizeTool preset - default.lua
@@ -0,0 +1,6 @@
+-- @description Set QuantizeTool preset to 'default'
+-- @author MPL
+-- @website https://forum.cockos.com/showthread.php?t=188335
+
+reaper.SetExtState("MPL_QuantizeTool", "ext_strategy_name", "default",false)
+reaper.SetExtState("MPL_QuantizeTool","ext_state",1,false)
