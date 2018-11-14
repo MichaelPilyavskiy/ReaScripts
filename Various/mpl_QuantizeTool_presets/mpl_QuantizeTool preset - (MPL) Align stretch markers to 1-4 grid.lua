c884afaa99596e7e2f362578c3fe66b8e1f46228
Michael <m.pilyavskiy@gmail.com>
2018-11-14 09:12:44 +0300

    test2

--- Various/mpl_QuantizeTool_presets/mpl_QuantizeTool preset - (MPL) Align stretch markers to 1-4 grid.lua
+++
@@ -1,7 +0,0 @@
--- @description mpl_QuantizeTool preset - (MPL) Align stretch markers to 1-4 grid
--- @author MPL
--- @website https://forum.cockos.com/showthread.php?t=188335
--- @noindex
-
-reaper.SetExtState("MPL_QuantizeTool", "ext_strategy_name", "(MPL) Align stretch markers to 1-4 grid",false)
-reaper.SetExtState("MPL_QuantizeTool","ext_state",1,false)
