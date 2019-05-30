d6bdd347a5b570deca4b5271a67ea36948476816
Michael <m.pilyavskiy@gmail.com>
2019-05-30 17:04:46 +0300

    fix7

---
+++ FX specific/mpl_RS5k tools.lua
@@ -0,0 +1,25 @@
+-- @description mpl_RS5k tools
+-- @version 1.0
+-- @author MPL
+-- @website http://forum.cockos.com/showthread.php?t=188335
+-- @metapackage
+-- @provides
+--    [main] mpl_RS5k tools/mpl_Add or replace source of item under mouse cursor for RS5k instance on selected track.lua
+--    [main] mpl_RS5k tools/mpl_Add or replace source of selected item for RS5k instance on its track.lua
+--    [main] mpl_RS5k tools/mpl_Add or replace source of selected item for RS5k instance on selected track.lua
+--    [main] mpl_RS5k tools/mpl_Add or replace source of selected item for RS5k instance on track under mouse cursor.lua
+--    [main] mpl_RS5k tools/mpl_Explode selected track RS5k instances to new tracks.lua
+--    [main] mpl_RS5k tools/mpl_Export selected item to RS5k instance on same track as chromatic source.lua
+--    [main] mpl_RS5k tools/mpl_Export selected items to RS5k instances on selected track (drum mode).lua
+--    [main] mpl_RS5k tools/mpl_Export selected items to RS5k instances on selected track (use original source).lua
+--    [main] mpl_RS5k tools/mpl_Export selected items to RS5k instances on selected track.lua
+--    [main] mpl_RS5k tools/mpl_Export selected items to single RS5k instance on selected track.lua
+--    [main] mpl_RS5k tools/mpl_Float RS5k related to input note on last touched track.lua
+--    [main] mpl_RS5k tools/mpl_List next sample in directory for focused RS5k.lua
+--    [main] mpl_RS5k tools/mpl_List next sample in directory for RS5k on selected track.lua
+--    [main] mpl_RS5k tools/mpl_List previous sample in directory for focused RS5k.lua
+--    [main] mpl_RS5k tools/mpl_List previous sample in directory for RS5k on selected track.lua
+--    [main] mpl_RS5k tools/mpl_List random sample in directory for focused RS5k.lua
+--    [main] mpl_RS5k tools/mpl_List samples in directory for focused RS5k (MIDI, OSC, Mousewheel).lua
+--    [main] mpl_RS5k tools/mpl_Open and Close Media Explorer when single item is inserted, export chromatically to RS5k (background).lua
+--    [main] mpl_RS5k tools/mpl_Open and Close Media Explorer when single item is inserted, export it to RS5k (background).lua
