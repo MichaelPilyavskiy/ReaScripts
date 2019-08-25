395f97872cf1f307f6be47ab969cae211119a4e4
Michael <m.pilyavskiy@gmail.com>
2019-08-25 06:33:46 +0300

    2508

--- FX specific/mpl_Create ReaComp sidechain routing.lua
+++ FX specific/mpl_Create ReaComp sidechain routing.lua
@@ -1,67 +1,76 @@
 -- @description Create ReaComp sidechain routing from selected track to track under mouse cursor
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # ReaPack header, name
+--    # support for new track under mouse catching
+--    # collect GUIDs instead indexes
+--    # properly increase dest track channel count, if need
+--    # avoid chunking
+--    # avoid doubled sends
   
-  script_title = "Create ReaComp sidechain routing"
   
   threshold = 0.25
   ratio = 0.06
+  defsendvol = 1
         
-  -- create_ReaComp_sidechain_routing
-  -- source - selected track
-  -- destination - track under mouse cursor
+  --------------------------------------------------------------------------------------
+  function main(threshold, ratio, defsendvol)
   
-  reaper.Undo_BeginBlock()
+    -- get source
+      src_tr = {}
+      for tr_i = 1, CountSelectedTracks(0) do
+        track = GetSelectedTrack(0,tr_i-1)
+        src_tr[#src_tr+1] = GetTrackGUID( track )
+      end 
+      if #src_tr == 0 then return end
   
-  -- get source tracks id`s--
-    count_tracks = reaper.CountTracks(0)
-    if count_tracks ~= nil then
-      sel_id_t = {}
-      for i = 1, count_tracks do
-        track = reaper.GetTrack(0,i-1)
-        isselect = reaper.IsTrackSelected(track)
-        if isselect == true then 
-          i_table = i - 1     
-          table.insert(sel_id_t, i_table)
+    -- get dest
+      dest_tr = VF_GetTrackUnderMouseCursor()
+      if not dest_tr then return end
+      dest_trGUID = GetTrackGUID( dest_tr )
+
+    -- increase chan
+      ch_cnt = GetMediaTrackInfo_Value( dest_tr, 'I_NCHAN' )
+      SetMediaTrackInfo_Value( dest_tr, 'I_NCHAN', math.max(4, ch_cnt) )
+      
+    -- insert reacomp
+      local reacompid = TrackFX_AddByName( dest_tr, 'ReaComp (Cockos)', false, 1 )
+      TrackFX_SetOpen(dest_tr, reacompid, true)
+      TrackFX_SetParam(dest_tr, reacompid, 0, threshold)
+      TrackFX_SetParam(dest_tr, reacompid, 1, ratio)    
+      TrackFX_SetParam(dest_tr, reacompid, 8, (1/1084)*2)  
+    
+    -- add sends                  
+      for i = 1, #src_tr do
+        if src_tr[i] ~= dest_trGUID then
+          local src_tr =  BR_GetMediaTrackByGUID( 0, src_tr[i] )
+          
+          local new_id
+          -- check for existing id
+          for sid = 1, GetTrackNumSends( src_tr, 0 ) do
+            local dest_tr_pointer = BR_GetMediaTrackSendInfo_Track( src_tr, 0,  sid-1, 1 )
+            local dest_tr_pointerGUID = GetTrackGUID(dest_tr_pointer)
+            if dest_tr_pointerGUID == dest_trGUID then new_id = sid-1 break end
+          end
+          
+          if not new_id then new_id = CreateTrackSend( src_tr, dest_tr ) end
+          SetTrackSendInfo_Value( src_tr, 0, new_id, 'D_VOL', defsendvol)
+          SetTrackSendInfo_Value( src_tr, 0, new_id, 'I_DSTCHAN', 2) -- 3/4
         end
-      end  
-    end
-  if reaper.CountSelectedTracks(0) == 1 then 
-  -- get dest track info
-    window, segment, details = reaper.BR_GetMouseCursorContext()
-    if segment == "track" then
-      dest_track = reaper.BR_GetMouseCursorContext_Track()
-      retval, track_state_chunk = reaper.GetTrackStateChunk(dest_track, "")
-    end  
-  
-  -- form temp string --
-    if sel_id_t ~= nil and #sel_id_t > 0 then
-      temp_string = ""
-      for i = 1, #sel_id_t do
-        source_track_id = sel_id_t[i]
-        temp_string = temp_string.."AUXRECV "..source_track_id.." 3 1 0 0 0 0 0 2 -1 4177951 -1 ''".."\n"
       end
-    end
+    
+  end
+  ---------------------------------------------------------------------
+  function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
+  --------------------------------------------------------------------  
+  local ret = CheckFunctions('VF_CalibrateFont') 
+  local ret2 = VF_CheckReaperVrs(5.982,true)    
+  if ret and ret2 then 
+    Undo_BeginBlock()
+    main(threshold, ratio, defsendvol)
+    Undo_EndBlock('Create ReaComp sidechain routing', -1)
+  end   
   
-  -- set new chunk --
-    if dest_track ~= nil then
-      newchunk = string.gsub(track_state_chunk, "MIDIOUT", temp_string.."MIDIOUT")
-      newchunk = string.gsub(newchunk, 'NCHAN 2','NCHAN 4')
-      reaper.SetTrackStateChunk(dest_track, newchunk)
-            
-      --insert reacomp
-        reaper.TrackFX_GetByName(dest_track, 'ReaComp (Cockos)', true)
-        fx_id = reaper.TrackFX_GetByName(dest_track, 'ReaComp (Cockos)', false)
-        reaper.TrackFX_SetOpen(dest_track, fx_id, true)
-        reaper.TrackFX_SetParam(dest_track, fx_id, 0, threshold)
-        reaper.TrackFX_SetParam(dest_track, fx_id, 1, ratio)    
-        reaper.TrackFX_SetParam(dest_track, fx_id, 8, (1/1084)*2)    
-    end  
-   end 
-  --  reaper.ShowConsoleMsg(track_state_chunk)
   
-  reaper.UpdateArrange()
-  reaper.Undo_EndBlock(script_title, 0)
+  
--- FX/mpl_Create send from focused FX insert.lua
+++ FX/mpl_Create send from focused FX insert.lua
@@ -1,18 +1,17 @@
 -- @description Create send from focused FX insert
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---   # respect send parameters
---   # forum link
+--   # Native remove FX implementation
 
-  for key in pairs(reaper) do _G[key]=reaper[key]  end  
   
-  local defsendvol = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  get_ini_file() )})[2]
-  local defsendflag = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendflag', '0',  get_ini_file() )})[2]
     
-  function msg(s)  ShowConsoleMsg(s..'\n') end
   function main()
+    
+    local defsendvol = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  get_ini_file() )})[2]
+    local defsendflag = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendflag', '0',  get_ini_file() )})[2]
+    
     local ret, tracknumberOut, _, fxnumberOut = GetFocusedFX()
     if not ret or tracknumberOut < 1 or fxnumberOut < 0 then return end
     
@@ -35,12 +34,19 @@ DOCKED 0]]..'\n'..insert_chunk..'\n>\n>'
     GetSetMediaTrackInfo_String( new_tr, 'P_NAME', 'AUX '..fxname:match('[%:](.*)'):sub(2), 1 )
     
     -- remove old inssert fx
-    if ret then SNM_MoveOrRemoveTrackFX( track, fxnumberOut, 0 ) end
+    if ret then  reaper.TrackFX_Delete(track, fxnumberOut ) end
     new_id = CreateTrackSend( track, new_tr )
     SetTrackSendInfo_Value( track, 0, new_id, 'D_VOL', defsendvol)
     SetTrackSendInfo_Value( track, 0, new_id, 'I_SENDMODE', defsendflag)
   end
-  
-  Undo_BeginBlock()
-  main()
-  Undo_EndBlock('Create send from focused FX insert', 0)
+
+  ---------------------------------------------------------------------
+    function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
+  --------------------------------------------------------------------  
+    local ret = CheckFunctions('VF_CalibrateFont') 
+    local ret2 = VF_CheckReaperVrs(5.95,true)    
+    if ret and ret2 then 
+      Undo_BeginBlock()
+      main()
+      Undo_EndBlock('Create send from focused FX insert', -1)
+    end    
