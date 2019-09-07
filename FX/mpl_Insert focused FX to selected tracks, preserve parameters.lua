a066643920c79f45c743fdec61ec7bdd340df568
Michael <m.pilyavskiy@gmail.com>
2019-09-07 08:59:06 +0300

    0709

--- FX/mpl_Insert focused FX to selected tracks, preserve parameters.lua
+++ FX/mpl_Insert focused FX to selected tracks, preserve parameters.lua
@@ -1,39 +1,27 @@
--- @version 1.0
--- @author MPL
--- @changelog
---   + init release
 -- @description Insert focused FX to selected tracks, preserve parameters
--- @website http://forum.cockos.com/member.php?u=70694
-  
+-- @version 1.01
+-- @author MPL
+-- @website http://forum.cockos.com/showthread.php?t=188335
+-- @changelog
+--    + obey master track
+--    + use native direct copy FX API (REAPER 5.95+)
+
   function main()
-    ret, tracknumber, _, fxnumberOut = reaper.GetFocusedFX()
+    local ret, tracknumber, _, src_fx = GetFocusedFX()
     if ret == 0 then return end
-    local track = reaper.CSurf_TrackFromID(tracknumber, false)
-    _, fxname = reaper.TrackFX_GetFXName( track, fxnumberOut, '' )
+    local src_track = CSurf_TrackFromID(tracknumber, false)
+    if tracknumber ==0 then src_track =  GetMasterTrack( 0 ) end
     
-    local parm_t = {}
-    for i = 1, reaper.TrackFX_GetNumParams( track, fxnumberOut ) do
-      local val = reaper.TrackFX_GetParam( track, fxnumberOut, i-1 )
-      parm_t[#parm_t+1] = val
-    end
-    
-    if fxname:find('/') then 
-      fxname = fxname:reverse()
-      fxname = fxname:sub(0,fxname:find('/') - 1)
-      fxname = fxname:reverse()
-     else
-      fxname = fxname:match('%:.+'):sub(3)
-    end
-    
-    for sel_tr = 1,  reaper.CountSelectedTracks( 0 ) do
-      sel_track = reaper.GetSelectedTrack( 0, sel_tr-1 )
-      if sel_track ~= track then 
-        new_fx_id = reaper.TrackFX_AddByName( sel_track, fxname, false, -1 ) 
-        for i = 1, reaper.TrackFX_GetNumParams( sel_track, new_fx_id ) do        
-          reaper.TrackFX_SetParam( sel_track, new_fx_id, i-1, parm_t[i] )
-        end        
+    for sel_tr = 1,  CountSelectedTracks( 0 ) do
+      local dest_track = GetSelectedTrack( 0, sel_tr-1 )
+      if dest_track ~= track then 
+        TrackFX_CopyToTrack( src_track, src_fx, dest_track, TrackFX_GetCount( dest_track ), false )    
       end
-    end
-  end
-  
-  main()
+    end  
+  end  
+---------------------------------------------------------------------
+  function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
+  --------------------------------------------------------------------  
+    local ret = CheckFunctions('VF_CalibrateFont') 
+    local ret2 = VF_CheckReaperVrs(5.95,true)    
+    if ret and ret2 then main() end
