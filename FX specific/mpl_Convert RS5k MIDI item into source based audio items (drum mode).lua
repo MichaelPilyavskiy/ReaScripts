112b63231e2bd1b3bb97250ab2360e81eebf990a
Michael <m.pilyavskiy@gmail.com>
2019-11-04 17:54:43 +0300

    0411

--- FX specific/mpl_Convert RS5k MIDI item into source based audio items (drum mode).lua
+++ FX specific/mpl_Convert RS5k MIDI item into source based audio items (drum mode).lua
@@ -1,9 +1,11 @@
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @description Convert RS5k MIDI item into source based audio items (drum mode)
 -- @changelog
---    # obey multiple rs5k instances
+--    # obey pitches / length
+--    # obey sample offset
+--    # obey sample pan
 
   local scr_nm = 'Convert RS5k MIDI item into source based audio items (drum mode)'
   for key in pairs(reaper) do _G[key]=reaper[key]  end 
@@ -22,6 +24,10 @@
         local retval, buf = TrackFX_GetParamName( tr, fx-1, 3, '' )
         if retval and buf == 'Note range start' then
           local pitch = math.floor(TrackFX_GetParam( tr, fx-1, 3 )  * 128)
+          local pitch_offset = TrackFX_GetParam( tr, fx-1, 15)
+          local start_offs = TrackFX_GetParamNormalized(  tr, fx-1, 13 )
+          local end_offs = TrackFX_GetParamNormalized(  tr, fx-1, 14 )
+          local pan = TrackFX_GetParamNormalized(  tr, fx-1, 1 )
           local retval2, sample_path = reaper.TrackFX_GetNamedConfigParm( tr, fx-1, 'FILE' )
           if retval2 then 
             local src =  PCM_Source_CreateFromFileEx( sample_path, false )
@@ -29,7 +35,13 @@
             if not src then return end
             p_map[pitch] = {spl = sample_path,
                             src=src,
-                            srclen=srclen}
+                            srclen=srclen,
+                            pitch_offset=(pitch_offset-0.5)*160,
+                            start_offs=start_offs,
+                            end_offs=end_offs,
+                            pan=pan
+                            }
+                            
           end
         end
       end
@@ -54,11 +66,18 @@
         local take = AddTakeToMediaItem( new_item )
         local p0 = data[i].pitch
         if p_map[p0] then
+          local prate = 2^(p_map[p0].pitch_offset/12) 
           SetMediaItemTake_Source( take, p_map[p0].src )
+          
           SetMediaItemInfo_Value( new_item, 'B_MUTE', data[i].mute )
           SetMediaItemInfo_Value( new_item, 'D_VOL', data[i].vol )
           SetMediaItemInfo_Value( new_item, 'D_POSITION', data[i].pos )
-          SetMediaItemInfo_Value( new_item, 'D_LENGTH', p_map[p0].srclen ) 
+          SetMediaItemInfo_Value( new_item, 'D_LENGTH', (p_map[p0].end_offs - p_map[p0].start_offs)  * p_map[p0].srclen/prate   ) 
+          SetMediaItemTakeInfo_Value( take, 'D_STARTOFFS', p_map[p0].start_offs   * p_map[p0].srclen) 
+          SetMediaItemTakeInfo_Value( take, 'D_PAN', (p_map[p0].pan-0.5)*2) 
+          SetMediaItemTakeInfo_Value( take, 'D_PITCH',  p_map[p0].pitch_offset )
+          SetMediaItemTakeInfo_Value( take, 'D_PLAYRATE', prate)
+           
         end
       end
     
@@ -68,4 +87,4 @@
   Undo_BeginBlock2( 0 )
   item = GetSelectedMediaItem(0,0)
   ConvertRS5kMIDI2AudioItems(item)
-  reaper.Undo_EndBlock2( 0, scr_nm, 0 )
+  Undo_EndBlock2( 0, scr_nm, 0 )
