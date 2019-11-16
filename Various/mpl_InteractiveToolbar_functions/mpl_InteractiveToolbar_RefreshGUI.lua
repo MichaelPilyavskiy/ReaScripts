cb2f3c8bbf673a0d2b0175b77e68a212be97678d
Michael <m.pilyavskiy@gmail.com>
2019-11-16 22:43:00 +0300

    1.01

--- Various/mpl_InteractiveToolbar_functions/mpl_InteractiveToolbar_RefreshGUI.lua
+++ Various/mpl_InteractiveToolbar_functions/mpl_InteractiveToolbar_RefreshGUI.lua
@@ -1,9 +1,11 @@
 -- @description InteractiveToolbar_refreshGUI
 -- @author MPL
+-- @version 1.01
 -- @website http://forum.cockos.com/member.php?u=70694
+-- @changelog
+--    + init
 
 
 
 reaper.gmem_attach('MPLInterToolbar')
 reaper.gmem_write(1,1)
-
