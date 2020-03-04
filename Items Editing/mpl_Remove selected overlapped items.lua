6278fc65a4c116c091241984bc8640ed7bf904cd
Michael <m.pilyavskiy@gmail.com>
2020-03-05 01:01:48 +0300

    rsoi101

--- Items Editing/mpl_Remove selected overlapped items.lua
+++ Items Editing/mpl_Remove selected overlapped items.lua
@@ -1,28 +1,33 @@
--- @version 1.0
+-- @version 1.01
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @description Remove selected overlapped items
 -- @changelog
---    + init
+--    # correct algorithm
 
   
   function main()
     local t = {}
     for i = 1, reaper.CountSelectedMediaItems(0) do
       local item = reaper.GetSelectedMediaItem( 0, i-1 )      
-      t[#t+1] = {pos = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' ),
+      local pos = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' )
+      local len = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
+      t[#t+1] = {pos=pos,
+                end_pos = pos+len,
                 guid = reaper.BR_GetMediaItemGUID( item )}
     end
     
+    ittoremove = {}
     for i = 1, #t do
       if t[i] then
         local pos_cur = t[i].pos
+        local end_pos_cur = t[i].end_pos
         for j = 1, #t do
           if t[j] and j ~= i then
             local pos_check = t[j].pos
-            if math.abs(pos_check-pos_cur) < 0.001 then
-              local item = reaper.BR_GetMediaItemByGUID( 0, t[j].guid )
-              if item then reaper.DeleteTrackMediaItem(  reaper.GetMediaItem_Track( item ), item ) end
+            local end_pos_check = t[j].end_pos
+            if HasCross(pos_cur,end_pos_cur,pos_check,end_pos_check) then
+              ittoremove[#ittoremove+1] = t[j].guid
               t[j] = nil
             end
           end
@@ -30,7 +35,21 @@
       end
     end
     
+    for i = 1, #ittoremove do
+      local item = reaper.BR_GetMediaItemByGUID( 0, ittoremove[i] )
+      if item then reaper.DeleteTrackMediaItem(  reaper.GetMediaItem_Track( item ), item ) end
+    end
+    
     reaper.UpdateArrange()
+  end
+  
+  function HasCross(p1,p2, p3, p4)
+    if  (p1<=p3 and p2>=p3) or 
+        (p1<=p4 and p2>=p4) or 
+        (p1>p3 and p2<p4) or
+        (p1<p3 and p2>p4) then
+      return true
+    end
   end
   
   reaper.Undo_BeginBlock()
