14779eec07d96d9975a1b80160c343393584e8b6
Michael <m.pilyavskiy@gmail.com>
2020-04-11 10:50:12 +0300

    1104

--- FX/mpl_Move focused Track FX to screen center.lua
+++ FX/mpl_Move focused Track FX to screen center.lua
@@ -1,98 +1,53 @@
--- @version 1.0
--- @author MPL
--- @changelog
---   + init
 -- @description Move focused Track FX to screen center
--- @website http://forum.cockos.com/member.php?u=70694
-
---[[
-   * ReaScript Name: Move focused Track FX to screen center
-   * Lua script for Cockos REAPER
-   * Author: Michael Pilyavskiy (mpl)
-   * Author URI: http://forum.cockos.com/member.php?u=70694
-   * Licence: GPL v3
-  ]]
+-- @version 1.01
+-- @author MPL
+-- @website http://forum.cockos.com/showthread.php?t=188335
+-- @changelog
+--    # improve fix chunk modification
   
 -------------------------------------------------
-  
-  function msg(s) reaper.ShowConsoleMsg(s) end
-  
   function main()
-    local track, fx_guid, chunk, ch_t,ch_start_id,ch_end_id, float_id
-    local     retval, tracknumber, itemnumber, fxnumber = reaper.GetFocusedFX()
-    
-    if retval == 0 then return end 
-    
-    -- if track FX
-      if retval == 1 then
-        if tracknumber == 0 then 
-          track = reaper.GetMasterTrack( 0 )
-         else
-          track = reaper.GetTrack(0, tracknumber -1 )
-        end
-        if track == nil then return end
-         fx_guid = reaper.TrackFX_GetFXGUID( track, fxnumber )
-        _, chunk = reaper.GetTrackStateChunk( track, '' )
-        
-        
-        -- get chunk t
-          ch_t = {}
-          for line in chunk:gmatch('[^\r\n]+') do
-            ch_t[#ch_t+1] = line
-          end
-          
-        -- get fx_chian st
-          for i = 1, #ch_t do
-            if ch_t[i]: find("FXCHAIN") ~= nil then
-              ch_start_id = i 
-              break
-            end
-          end
-          
-        -- get fx chain end
-          local level = 0 
-          for i = ch_start_id, #ch_t do
-          
-            if ch_t[i]:find('<') then  
-              level = level + 1 
-            end
-            
-            if  ch_t[i]:find('>') then 
-              level = level - 1 
-            end
-            
-            if level == 0 then ch_end_id = i break end
-          end
-          
-        -- get line with xywh
-          local fx_id = -1
-          for i = ch_start_id, ch_end_id do
-            if ch_t[i] : find("FLOAT") then fx_id = fx_id + 1 end
-            if fx_id == fxnumber then float_id = i break end
-          end
-          
-          --msg(ch_t[float_id])
-          param_line = ch_t[float_id]
-          temp_t = {}
-          for word in param_line:gmatch('[^%s]+') do
-            temp_t [#temp_t+1] = tonumber(word)
-          end
-          
-          _, _, scr_w, scr_h = reaper.my_getViewport(0,0,0,0,0,0,0,0, true)
-          
-          temp_t[1] = math.ceil((scr_w - temp_t[3]) /2)
-          temp_t[2] = math.ceil((scr_h - temp_t[4]) /2)
-          
-          ch_t[float_id] = 'FLOAT '..table.concat(temp_t, ' ')
-          
-          chunk_out = table.concat(ch_t, '\n')
-          --msg(chunk_out)
-          
-          reaper.SetTrackStateChunk( track, chunk_out )
-      end
-    
+    local track
+    local retval, tracknumber, itemnumber, fxnumber = GetFocusedFX()
+    if retval ~= 1 then return end 
+    if tracknumber == 0 then tr = GetMasterTrack( 0 ) else tr = GetTrack(0, tracknumber -1 ) end
+    if not tr then return end
+    Data_ModifyFloat(tracknumber,fxnumber)
   end
-  
-  --msg("")
-  main()
+  -----------------------------------------------
+  function Data_ModifyFloat(trid,fx)
+    local tr= GetTrack(0,trid-1)
+    if not tr then return end
+    local retval, tr_chunk = GetTrackStateChunk( tr, '', false )
+    local fxGUID_check = TrackFX_GetFXGUID( tr, fx )
+    for fxchunk in tr_chunk:gmatch('(BYPASS.-WAK %d)') do
+      local fxGUID = fxchunk:match('FXID (.-)\n')
+      if not fxGUID then fxGUID = fxchunk:match('FXID_NEXT (.-)\n') end
+
+      if fxGUID:match(literalize(fxGUID_check):gsub('%s', '')) then
+        local fxchunk_param_line = fxchunk:match('(FLOAT .-)\n')
+        if not fxchunk_param_line then fxchunk_param_line = fxchunk:match('(FLOATPOS .-)\n') end
+         temp_t = {}
+        for num in fxchunk_param_line:gmatch('[^%s]+') do temp_t [#temp_t+1] = tonumber(num) end --table.remove(temp_t,0)
+        local _, _, scr_w, scr_h = reaper.my_getViewport(0,0,0,0,0,0,0,0, true)
+        temp_t[1] = math.ceil((scr_w - temp_t[3]) /2)
+        temp_t[2] = math.ceil((scr_h - temp_t[4]) /2)
+        local outstr = 'FLOAT '..table.concat(temp_t, ' ')
+          
+        fxchunk_mod = fxchunk:gsub(fxchunk_param_line, outstr..'\n')
+                  
+        tr_chunk = tr_chunk:gsub(literalize(fxchunk), fxchunk_mod)
+        SetTrackStateChunk( tr, tr_chunk, false )
+        return
+      end
+    end
+  end  
+---------------------------------------------------------------------
+  function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
+ --------------------------------------------------------------------  
+  local ret = CheckFunctions('VF_CalibrateFont') 
+  if ret then
+    local ret2 = VF_CheckReaperVrs(5.95,true)    
+    if ret and ret2 then main() end
+  end
   
