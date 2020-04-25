4fd36e64af962370bc1837fd5da38bfc0c5f3707
Michael <m.pilyavskiy@gmail.com>
2020-04-25 14:48:55 +0300

    2504

--- Tracks Properties/mpl_Add receive track from selected tracks.lua
+++ Tracks Properties/mpl_Add receive track from selected tracks.lua
@@ -1,31 +1,29 @@
--- @description Add send to selected tracks
--- @version 1.01
+-- @description Add receive track from selected tracks
+-- @version 1.0
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # respect default send parameters
---    # forum link
---    - don`t set same color
-  local defsendvol = ({reaper.BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  reaper.get_ini_file() )})[2]
+--    # change name
+
+ local defsendvol = ({reaper.BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  reaper.get_ini_file() )})[2]
   local defsendflag = ({reaper.BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendflag', '0',  reaper.get_ini_file() )})[2]
     
   function main()
-    local retval, pref = reaper.GetUserInputs( 'Set send prefix', 1, '' ,'')
-    local retval1, suff = reaper.GetUserInputs( 'Set send suffix', 1, '' ,'')
+    local retval, sname = reaper.GetUserInputs( 'New name', 1, '' ,'')
     if retval or retval1 then 
       for i = 1, reaper.CountSelectedTracks(0) do
         local tr = reaper.GetSelectedTrack(0,i-1)
         if tr then 
           track_col =  reaper.GetTrackColor( tr )
-          _, src_name = reaper.GetSetMediaTrackInfo_String( tr, 'P_NAME', '',false )
-          reaper.InsertTrackAtIndex( reaper.GetNumTracks(0), true )
-          new_dest_tr =  reaper.CSurf_TrackFromID( reaper.GetNumTracks(0), false )
+          if i == 1 then 
+            reaper.InsertTrackAtIndex( reaper.GetNumTracks(0), true )
+            new_dest_tr =  reaper.CSurf_TrackFromID( reaper.GetNumTracks(0), false )
+          end
           
           new_send_id = reaper.CreateTrackSend( tr, new_dest_tr)
           if new_send_id >= 0 then
-            new_name = pref..' '..src_name..' '..suff
             if new_dest_tr then
-              reaper.GetSetMediaTrackInfo_String( new_dest_tr, 'P_NAME', new_name ,true)
+              reaper.GetSetMediaTrackInfo_String( new_dest_tr, 'P_NAME', sname ,true)
               reaper.SetTrackSendInfo_Value( tr, 0, new_send_id, 'D_VOL', defsendvol)
               reaper.SetTrackSendInfo_Value( tr, 0, new_send_id, 'I_SENDMODE', defsendflag)
             end
@@ -37,4 +35,3 @@
   end
   
   main()
-  
