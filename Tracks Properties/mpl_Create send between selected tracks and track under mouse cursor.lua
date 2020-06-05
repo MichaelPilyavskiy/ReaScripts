57a3a566ae4ff9b2cff81b416260725b8da22082
mpl <m.pilyavskiy@gmail.com>
2020-06-05 23:05:23 +0300

    0506

--- Tracks Properties/mpl_Create send between selected tracks and track under mouse cursor.lua
+++ Tracks Properties/mpl_Create send between selected tracks and track under mouse cursor.lua
@@ -1,6 +1,6 @@
--- @version 1.11
+-- @version 1.12
 -- @author MPL
--- @description Send selected tracks to track under mouse cursor
+-- @description Create send between selected tracks and track under mouse cursor
 -- @metapackage
 -- @provides
 --    [main] . > mpl_Send selected tracks to track under mouse cursor (multichannel).lua
@@ -19,9 +19,27 @@
 --    [main] . > mpl_Send selected tracks to track under mouse cursor (channel 11-12 to 1-2).lua
 --    [main] . > mpl_Send selected tracks to track under mouse cursor (channel 13-14 to 1-2).lua
 --    [main] . > mpl_Send selected tracks to track under mouse cursor (channel 15-16 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (multichannel).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 3-4).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 5-6).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 7-8).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 9-10).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 11-12).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 13-14).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 1-2 to 15-16).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 3-4 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks  (channel 5-6 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 7-8 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 9-10 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 11-12 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 13-14 to 1-2).lua
+--    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 15-16 to 1-2).lua
+-- @website http://forum.cockos.com/showthread.php?t=188335  
 -- @changelog
---    # update for use with REAPER 5.981+
--- @website http://forum.cockos.com/showthread.php?t=188335    
+--    # update name
+--    + allow to use as Send track under mouse cursor to selected tracks
+  
 
   
   ---------------------------------------------------------------------
@@ -53,8 +71,10 @@
     end
   end
   ---------------------------------------------------------------------   
-  function AddSends(src_t, dest_t, MCH_mode, src_ch, dest_ch, defsendvol, defsendflag)
-  
+  function AddSends(source_type, src_t0, dest_t0, MCH_mode, src_ch, dest_ch, defsendvol, defsendflag)
+    local src_t, dest_t = CopyTable(src_t0), CopyTable(dest_t0)
+    if source_type == false then  src_t, dest_t = CopyTable(dest_t0), CopyTable(src_t0) end
+    
     -- validate mode
       if MCH_mode==false and not (src_ch and dest_ch) then return end
     
@@ -100,13 +120,13 @@
     end
   end
   ---------------------------------------------------------------------  
-  function main(MCH_mode, src_ch, dest_ch, script_title, defsendvol, defsendflag)
+  function main(source_type, MCH_mode, src_ch, dest_ch, script_title, defsendvol, defsendflag)
     Undo_BeginBlock()
     local src_GUID = GetSrcTrGUID()
     local dest_GUID = GetDestTrGUID()    
     Check_t(src_GUID,dest_GUID)
     if #src_GUID < 1 or #dest_GUID < 1 then return end
-    AddSends(src_GUID,dest_GUID, MCH_mode, src_ch, dest_ch, defsendvol, defsendflag)
+    AddSends(source_type, src_GUID,dest_GUID, MCH_mode, src_ch, dest_ch, defsendvol, defsendflag)
     TrackList_AdjustWindows(false)
     Undo_EndBlock(script_title, 0) 
   end 
@@ -114,24 +134,27 @@
   function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
   ---------------------------------------------------------------------  
   function Parsing_filename()
-    local filename = ({reaper.get_action_context()})[2]           --:sub(0,-6)..' to 1-2).lua'
-    local script_title = filename:match('Send selected tracks to track under mouse cursor %(.*%)')
-    local mode = filename:match('Send selected tracks to track under mouse cursor %((.*)%)')
+    local filename = ({reaper.get_action_context()})[2]
+    local script_title = GetShortSmplName(filename):gsub('%.lua','')
+    local source_type = script_title:match('Send selected tracks to track under mouse cursor') ~= nil -- true==selected tracks is source
+    local channel_mode = script_title:match('%((.*)%)')
     local MCH_mode, dest_ch, src_ch = false
-    if mode:match('multichannel') then 
+    if channel_mode:match('multichannel') then 
       MCH_mode = true 
      else
-      src_ch = mode:match('channel (%d+)') if src_ch then src_ch = tonumber(src_ch) end
-      dest_ch = mode:match('to (%d+)') if dest_ch then dest_ch = tonumber(dest_ch) end
+      src_ch = channel_mode:match('channel (%d+)') if src_ch then src_ch = tonumber(src_ch) end
+      dest_ch = channel_mode:match('to (%d+)') if dest_ch then dest_ch = tonumber(dest_ch) end
     end
-    return MCH_mode, src_ch, dest_ch, script_title
+    return source_type, MCH_mode, src_ch, dest_ch, script_title
   end
   -------------------------------------------------
   local ret = CheckFunctions('VF_GetItemTakeUnderMouseCursor') 
-  local ret2 = VF_CheckReaperVrs(5.95)    
-  if ret and ret2 then 
-    local defsendvol = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  get_ini_file() )})[2]
-    local defsendflag = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendflag', '0',  get_ini_file() )})[2] 
-    local MCH_mode, src_ch, dest_ch, script_title = Parsing_filename()
-    main(MCH_mode, src_ch, dest_ch, script_title, defsendvol, defsendflag) 
+  if ret then 
+    local ret2 = VF_CheckReaperVrs(5.95)   
+    if ret then
+      local defsendvol = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendvol', '0',  get_ini_file() )})[2]
+      local defsendflag = ({BR_Win32_GetPrivateProfileString( 'REAPER', 'defsendflag', '0',  get_ini_file() )})[2] 
+      local source_type, MCH_mode, src_ch, dest_ch, script_title = Parsing_filename()
+      main(source_type, MCH_mode, src_ch, dest_ch, script_title, defsendvol, defsendflag) 
+    end
   end
