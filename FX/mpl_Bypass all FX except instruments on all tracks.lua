1b43759a7d346495f8658c332c2e9140aa05be27
mpl <m.pilyavskiy@gmail.com>
2020-06-07 12:18:07 +0300

    0706

--- FX/mpl_Bypass all FX except instruments on all tracks.lua
+++ FX/mpl_Bypass all FX except instruments on all tracks.lua
@@ -1,10 +1,9 @@
 -- @description Toggle bypass all FX except instruments on all tracks
--- @version 1.4
+-- @version 1.5
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694 
 -- @changelog
---    # fix initial state
---    + refresh toolbar
+--    # don`t bypass plugins standing before instrument (ex. MIDI tools)
 
   local scr_title = "Toggle Bypass all FX except instruments on all tracks"
   ---------------------------------------------------------
@@ -39,7 +38,10 @@
     end
     return t
   end
-  function IsInstrument(track, fx)  return  TrackFX_GetInstrument( track ) == fx end
+  function IsInstrument(track, fx)  
+    local fx_instr = TrackFX_GetInstrument( track )
+    return  fx_instr >= fx
+  end
   ---------------------------------------------------------  
   function SetFXState(FX_state, t)
     for i = 1, CountTracks(0) do
@@ -85,6 +87,8 @@
     function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
   
   --------------------------------------------------------------------  
-    local ret = CheckFunctions('VF_CalibrateFont') 
-    local ret2 = VF_CheckReaperVrs(5.95,true)    
-    if ret and ret2 then main() end
+    local ret = CheckFunctions('VF_CalibrateFont')  
+    if ret then
+      local ret2 = VF_CheckReaperVrs(5.95,true)  
+      if ret2 then main() end
+    end
