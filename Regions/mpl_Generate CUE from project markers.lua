8156c6584ea579b215682c21dad6f8e9337143b9
mpl <m.pilyavskiy@gmail.com>
2021-08-11 22:57:38 +0300

    1108

--- Regions/mpl_Generate CUE from project markers.lua
+++ Regions/mpl_Generate CUE from project markers.lua
@@ -1,25 +1,28 @@
 -- @description Generate CUE from project markers
--- @version 1.03
+-- @version 1.04
 -- @author MPL
 -- @website https://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    + Add save file dialog (require JS ReaScript API Extension)
+--    + Obey markers only inside time selection
+--    + Do not add markers with # sign at the start
+--    + Skip executing if user cancelled dialog
+--    # require VariousFunctions v2+
   
   
   function main()
     local scr_name = 'MPL Generate CUE from markers'
-    local _, cnt_markers =  reaper.CountProjectMarkers(0)
-    if not cnt_markers or cnt_markers == 0 then reaper.MB('Add markers to project first', scr_name, 0) return end
+    local _, cnt_markers =  CountProjectMarkers(0)
+    if not cnt_markers or cnt_markers == 0 then MB('Add markers to project first', scr_name, 0) return end
     
-    reaper.ClearConsole()
-    local _, user_inputs = reaper.GetUserInputs('Cue', 5, 
+    local ret, user_inputs = GetUserInputs('Cue', 5, 
         'Genre,Year,Performer,Album Title,File name (with extension)', 
         'Other,2016,Performer,Album_Title,FileName.wav')
+    if not ret then return end
     local fields = {}
     for word in user_inputs:gmatch('[^%,]+') do fields[#fields+1] = word end
-    if #fields ~= 5 then reaper.MB('Empty fields not supported', scr_name, 0) return end
+    if #fields ~= 5 then MB('Empty fields not supported', scr_name, 0) return end
     local ext_len = fields[5]:reverse():find('%.')
-    if not ext_len then reaper.MB('Enter filename with extension', scr_name, 0) return end
+    if not ext_len then MB('Enter filename with extension', scr_name, 0) return end
     local extension = fields[5]:sub(1-ext_len):upper()
     
     out_str = 
@@ -32,10 +35,15 @@
     ind3 = '   '
     ind5 = '     '
 
+    -- loop markers
+    local tsstart, tsend = GetSet_LoopTimeRange2( 0, false, false, 0, 0, false )
+    
       for i = 1, cnt_markers do
       
-        _, _, posOut, _, nameOut, markrgnindexnumber = reaper.EnumProjectMarkers2(0, i-1)
-        posOut = reaper.format_timestr_pos(posOut, '', 5)
+        _, _, posOut, _, nameOut, markrgnindexnumber = EnumProjectMarkers2(0, i-1)
+        if not (posOut >= tsstart and posOut<=tsend) or nameOut:gsub('%s',''):sub(0,1) == '#' then goto skip_next end
+        
+        posOut = format_timestr_pos(posOut, '', 5)
         
         -- format hours/minutes to minutes 
           local time = {}
@@ -59,6 +67,7 @@
                            ind5..'TITLE '..'"'..nameOut1..'"'..'\n'..
                            ind5..'PERFORMER '..'"'..perf..'"'..'\n'..
                            ind5..'INDEX 01 '..posOut..'\n'
+        ::skip_next::
       end
       
     -- write to file
@@ -79,8 +88,11 @@
   ---------------------------------------------------------------------
   function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end 
   --------------------------------------------------------------------  
-  local ret = CheckFunctions('VF_CalibrateFont') 
-  local ret2 = VF_CheckReaperVrs(5.95,true)    
-  if ret and ret2 then 
-    if JS_Dialog_BrowseForSaveFile then main() else MB('Missed JS ReaScript API extension', 'Error', 0) end
+  local ret = CheckFunctions('VF2_LoadVFv2') 
+    
+  if ret then 
+    local ret2 = VF_CheckReaperVrs(5.95,true)  
+    if ret2 then 
+      if JS_Dialog_BrowseForSaveFile then main() else MB('Missed JS ReaScript API extension', 'Error', 0)  end
+    end
   end
