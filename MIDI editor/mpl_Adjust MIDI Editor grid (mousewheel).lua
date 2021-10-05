42fa729fa0e9c937bcf7fa151664a4c27d576903
mpl <m.pilyavskiy@gmail.com>
2021-10-05 21:40:17 +0300

    0510201

--- MIDI editor/mpl_Adjust MIDI Editor grid (mousewheel).lua
+++ MIDI editor/mpl_Adjust MIDI Editor grid (mousewheel).lua
@@ -1,12 +1,10 @@
 -- @description Adjust MIDI Editor grid (mousewheel) 
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---   # change name
---   # invert adjusting [p=2282406]
+--   # limit maximum to 1
  
-  function msg(s) reaper.ShowConsoleMsg(s..'\n')end
   function main()
     local ME= reaper.MIDIEditor_GetActive()
     if not ME then return end
@@ -16,7 +14,7 @@
     local _,_,_,_,_,_,mouse_scroll  = reaper.get_action_context()
     local dir = (mouse_scroll/math.abs(mouse_scroll))
     local out = grid*0.5^dir
-    if out >= 1/128 and out <= 8 then
+    if out >= 1/128 and out <= 1 then
       reaper.SetMIDIEditorGrid( 0,out  )
     end
   end
