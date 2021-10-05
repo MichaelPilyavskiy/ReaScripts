c9e6543c57bef975582ef71cc7c66cf4cb88c2c0
mpl <m.pilyavskiy@gmail.com>
2021-10-05 23:31:04 +0300

    0510_2

--- Various/mpl_Adjust arrange grid (mousewheel).lua
+++ Various/mpl_Adjust arrange grid (mousewheel).lua
@@ -1,17 +1,20 @@
 -- @description Adjust arrange grid (mousewheel) 
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---   # change name
+--   # limit to [1...1/1024]
  
  
   function main()
      _,_,_,_,_,_,mouse_scroll  = reaper.get_action_context() 
-    if mouse_scroll > 0 then 
-      reaper.Main_OnCommand(40783, 0)--Grid: Adjust by 1/2
+     retval, division, swingmode, swingamt = reaper.GetSetProjectGrid( 0, false, 0, 0, 0 )
+    if mouse_scroll > 0  then 
+      --reaper.Main_OnCommand(40783, 0)--Grid: Adjust by 1/2
+      reaper.GetSetProjectGrid( 0, true, math.max(division/2, 2^-10), swingmode, swingamt )
      elseif mouse_scroll < 0 then 
-      reaper.Main_OnCommand(40786, 0)--Grid: Adjust by 2
+      --reaper.Main_OnCommand(40786, 0)--Grid: Adjust by 2
+      reaper.GetSetProjectGrid( 0, true, math.min(1,division*2), swingmode, swingamt )
     end
   end
   
