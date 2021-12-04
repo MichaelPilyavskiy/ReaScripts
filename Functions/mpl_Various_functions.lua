df8b37efa605dc217b6b45978805469bdce235ac
mpl <m.pilyavskiy@gmail.com>
2021-12-04 17:09:51 +0300

    upd retrospective log

--- Functions/mpl_Various_functions.lua
+++ Functions/mpl_Various_functions.lua
@@ -2,7 +2,7 @@
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @about Functions used by MPL scripts.
--- @version 2.62
+-- @version 2.63
 -- @provides
 --    mpl_Various_functions_v1.lua
 --    mpl_Various_functions_v2.bin
@@ -12,9 +12,9 @@
 --    mpl_Various_functions_Pers.lua
 --    [main] mpl_Various_functions_PurchaseGUI.lua
 -- @changelog
---    + ExportSelItemsToRs5k_AddMIDI support no increment pitch
+--    + VF2_MPL_DumpRetrospectiveLog
     
-  VF_version = 2.62 -- do not remove, use for versions comparement
+  VF_version = 2.63 -- do not remove, use for versions comparement
   VF_isregist = 0 
   --------------------------------------------------
   function VF_LoadLibraries()
--- Various/mpl_Dump Retrospective Record log.lua
+++ Various/mpl_Dump Retrospective Record log.lua
@@ -1,93 +1,38 @@
 -- @description Dump Retrospective Record log
--- @version 1.01
--- @author MPL, Justin
+-- @version 2.0
+-- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
--- @about Dump recent MIDI messages log
+-- @about Dump recent MIDI messages log. 
+-- @metapackage
+-- @provides
+--    [main] . > mpl_Dump Retrospective Record log.lua
+--    [main] . > mpl_Dump Retrospective Record log (notes only).lua
+--    [main] . > mpl_Dump Retrospective Record log (only data at playing).lua
+--    [main] . > mpl_Dump Retrospective Record log (only data at stop).lua
+--    [main] . > mpl_Dump Retrospective Record log (everything recorded from last REAPER start).lua
+--    [main] . > mpl_Dump Retrospective Record log (everything from last 5 minutes).lua
+--    [main] . > mpl_Dump Retrospective Record log (everything from last 10 minutes).lua
+--    [main] . > mpl_Dump Retrospective Record log (everything from last 30 minutes).lua
+--    [main] . > mpl_Dump Retrospective Record log (everything from last hour, obey stored data break).lua
 -- @changelog 
---    # fix description, remove VF dependency
-
-
-  function DumpRetrospectiveLog_CollectEvents()
-    local t = {}
-    local i,last_ts,item_len_spls = 0,0,0
-    
-    while true do
-      local retval, buf, tsval, devIdx, projPos = reaper.MIDI_GetRecentInputEvent(i)
-      if retval == 0 then break end
-      if i > 0 and tsval < last_ts-4*48000 then break end -- 4sec of nothing, stop looking for more events
-      
-      i = i + 1
-      
-      if (devIdx & 0x10000) == 0 or devIdx == 0x1003e then
-        devIdx=devIdx&0xFFFF
-        if devIdx and not t[devIdx] then t[devIdx] = {} end
-        t[devIdx][#t[devIdx] + 1] = {msg1= buf, ts = tsval, pp=projPos}
-        last_ts = tsval
-      end
+--    # Internal cleanup and optimisation
+--    # Remove 4 seconds pause limit
+--    + Share with ReaPack as metadata package
+--    + Split data recorded at playing (on initialisation add take bounded to the exact stored events positions)
+--    + Split data recorded at stop (on initialisation add take to edit cursor)
+--    + Add script to add both takes with dumped data at play and at stop
+--    + Implement time stamp filter along with internal data breaks
+--    + Handle loop recorded data as multiple takes per mode (play/stop)
+--    + Add script to filter non-note data
+                    
+  ---------------------------------------------------------------------
+  function VF_CheckFunctions(vrs) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path) if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end  else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0)  if reaper.APIExists('ReaPack_BrowsePackages') then ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end    end
+  --------------------------------------------------------------------  
+  local ret = VF_CheckFunctions(2.63) if ret then local ret2 = VF_CheckReaperVrs(6.39,true) if ret2 then 
+    local settings = VF2_MPL_DumpRetrospectiveLog_Parsing_filename()
+    if settings then 
+      Undo_BeginBlock2( 0 )
+      VF2_MPL_DumpRetrospectiveLog(settings) 
+      Undo_EndBlock2( 0, 'MPL_DumpRetrospectiveLog', 0)
     end
-    for devIdx in pairs(t) do
-      for i = 1,#t[devIdx] do
-        local ts = t[devIdx][i].ts - last_ts
-        t[devIdx][i].ts = ts
-        item_len_spls = math.max(item_len_spls, ts)
-      end
-    end
-    return t,  item_len_spls
-  end
-  -------------------------------------------
-  function ApplyMIDIdata(take, midi_data_t, itempos, SR)
-    local midistr = ''
-    local ppq_cur, ppq_cur_last
-    local lpos = -1
-    for evt = #midi_data_t,1,-1 do
-      local evt_t = midi_data_t[evt]
-      local ppq_evt = math.floor(reaper.MIDI_GetPPQPosFromProjTime( take, itempos+ evt_t.ts / SR ))
-      ppq_cur = ppq_evt
-      if not ppq_cur_last then ppq_cur_last = ppq_cur end
-      local str_per_msg = string.pack("i4BI4BBB", ppq_cur - ppq_cur_last, 0, 3, evt_t.msg1:byte(1),evt_t.msg1:byte(2),evt_t.msg1:byte(3))
-      ppq_cur_last = ppq_cur
-      midistr = midistr..str_per_msg
-      if evt == #midi_data_t then lpos = evt_t.pp end
-    end
-    reaper.MIDI_SetAllEvts(take, midistr)
-    reaper.MIDI_Sort(take)
-    return lpos
-  end
-  -------------------------------------------
-  function DumpRetrospectiveLog()
-    -- get data
-      local SR = tonumber(reaper.format_timestr_pos( 1, '', 4 ))
-      local midi_t, item_len_spls = DumpRetrospectiveLog_CollectEvents()
-    
-    -- Create item at first selected track or new one if no track selected 
-      local track = reaper.GetSelectedTrack(0,0)
-      if not track then 
-        reaper.InsertTrackAtIndex(  reaper.CountTracks( 0 ), 1 )
-        track = reaper.GetTrack(0,reaper.CountTracks( 0 )-1)
-      end 
-    
-    -- Add item
-      local itempos = reaper.GetCursorPosition()
-      local item =  reaper.CreateNewMIDIItemInProj( track,  itempos,  itempos + item_len_spls / SR)
-    
-    -- pass data to item
-      local i = 1
-      for devIdx in pairs(midi_t) do
-        local retval, nameout = reaper.GetMIDIInputName( devIdx, '' )
-        local take = reaper.GetActiveTake( item )
-        if i ~= 1 then take = reaper.AddTakeToMediaItem( item ) end
-        local retval, nameout = reaper.GetMIDIInputName( devIdx, '' )
-        reaper.GetSetMediaItemTakeInfo_String( take, 'P_NAME', nameout, 1 )
-        local lpos = ApplyMIDIdata(take, midi_t[devIdx], itempos, SR)
-        if lpos >= 0 then
-          reaper.SetMediaItemPosition(item,lpos,false)
-        end
-        i = i + 1
-      end
-    
-    -- update arrange
-      reaper.UpdateArrange()
-  end
-  
-  if tonumber(reaper.GetAppVersion():match('[%d%.]+')) >= 6.39 then DumpRetrospectiveLog() end
-  
+  end end
