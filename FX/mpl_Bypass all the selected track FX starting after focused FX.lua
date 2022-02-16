38211ae529d1ca251bf34b4584497c79a1642ba6
mpl <m.pilyavskiy@gmail.com>
2022-02-16 22:13:41 +0300

    1602

---
+++ FX/mpl_Bypass all the selected track FX starting after focused FX.lua
@@ -0,0 +1,27 @@
+-- @description Bypass all the selected track FX starting after focused FX
+-- @version 1.0
+-- @author MPL
+-- @website https://forum.cockos.com/showthread.php?t=188335
+-- @changelog
+--    + init 
+
+  function main()
+    local retval, tracknumber, itemnumber, fxnumber = reaper.GetFocusedFX2()
+    if not (retval&1==1) then return end
+    if tracknumber == 0 then return end
+    local track = GetTrack(0,tracknumber-1)
+    if not track then return end
+    if fxnumber&0x1000000==0x1000000 then return end -- ignore input
+    for fx = fxnumber+1,  TrackFX_GetCount( track )-1 do
+       bypass_id = reaper.TrackFX_GetParamFromIdent( track, fx, ':bypass' )
+      TrackFX_SetParam( track, fx, bypass_id, 1 )
+    end 
+  end
+  ---------------------------------------------------------------------
+  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
+  --------------------------------------------------------------------  
+  local ret = VF_CheckFunctions(2.84) if ret then local ret2 = VF_CheckReaperVrs(5.975,true) if ret2 then 
+    Undo_BeginBlock2( 0 )
+    main() 
+    Undo_EndBlock2( 0, 'Bypass all the selected track FX starting after focused FX', 0 )
+  end end
