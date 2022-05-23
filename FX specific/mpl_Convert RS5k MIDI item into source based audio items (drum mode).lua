b86e31771a27823f09842cc70f2a4bb30efe861b
mpl <m.pilyavskiy@gmail.com>
2022-05-23 22:14:38 +0300

    2305

--- FX specific/mpl_Convert RS5k MIDI item into source based audio items (drum mode).lua
+++ FX specific/mpl_Convert RS5k MIDI item into source based audio items (drum mode).lua
@@ -1,11 +1,9 @@
--- @version 1.02
+-- @version 1.03
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @description Convert RS5k MIDI item into source based audio items (drum mode)
 -- @changelog
---    # obey pitches / length
---    # obey sample offset
---    # obey sample pan
+--    # obey MIDI notes length
 
   local scr_nm = 'Convert RS5k MIDI item into source based audio items (drum mode)'
   for key in pairs(reaper) do _G[key]=reaper[key]  end 
@@ -19,7 +17,7 @@
     local tr= GetMediaItem_Track( item )
     
     -- build pitch map
-      p_map = {}
+      local p_map = {}
       for fx = 1, TrackFX_GetCount( tr ) do
         local retval, buf = TrackFX_GetParamName( tr, fx-1, 3, '' )
         if retval and buf == 'Note range start' then
@@ -28,8 +26,8 @@
           local start_offs = TrackFX_GetParamNormalized(  tr, fx-1, 13 )
           local end_offs = TrackFX_GetParamNormalized(  tr, fx-1, 14 )
           local pan = TrackFX_GetParamNormalized(  tr, fx-1, 1 )
-          local retval2, sample_path = reaper.TrackFX_GetNamedConfigParm( tr, fx-1, 'FILE' )
-          if retval2 then 
+          local retval2, sample_path = reaper.TrackFX_GetNamedConfigParm( tr, fx-1, 'FILE0' )
+          if retval2 and sample_path then 
             local src =  PCM_Source_CreateFromFileEx( sample_path, false )
             local srclen = GetMediaSourceLength( src )
             if not src then return end
@@ -53,8 +51,10 @@
       for i = 1,  ({MIDI_CountEvts( take )})[2] do 
         local _, s, m, sppq, eppq, c,p, v =MIDI_GetNote( take, i-1 )
         if m then m = 1 else m = 0 end
+        local pos = MIDI_GetProjTimeFromPPQPos( take, sppq )
         data[#data+1] = {mute = m,
-                         pos =  MIDI_GetProjTimeFromPPQPos( take, sppq ),
+                         pos =  pos,
+                         len =  MIDI_GetProjTimeFromPPQPos( take, eppq )-pos,
                          vol = v/127,
                          pitch=p}
       end
@@ -68,11 +68,10 @@
         if p_map[p0] then
           local prate = 2^(p_map[p0].pitch_offset/12) 
           SetMediaItemTake_Source( take, p_map[p0].src )
-          
           SetMediaItemInfo_Value( new_item, 'B_MUTE', data[i].mute )
           SetMediaItemInfo_Value( new_item, 'D_VOL', data[i].vol )
           SetMediaItemInfo_Value( new_item, 'D_POSITION', data[i].pos )
-          SetMediaItemInfo_Value( new_item, 'D_LENGTH', (p_map[p0].end_offs - p_map[p0].start_offs)  * p_map[p0].srclen/prate   ) 
+          SetMediaItemInfo_Value( new_item, 'D_LENGTH', data[i].len)--(p_map[p0].end_offs - p_map[p0].start_offs)  * p_map[p0].srclen/prate   ) 
           SetMediaItemTakeInfo_Value( take, 'D_STARTOFFS', p_map[p0].start_offs   * p_map[p0].srclen) 
           SetMediaItemTakeInfo_Value( take, 'D_PAN', (p_map[p0].pan-0.5)*2) 
           SetMediaItemTakeInfo_Value( take, 'D_PITCH',  p_map[p0].pitch_offset )
