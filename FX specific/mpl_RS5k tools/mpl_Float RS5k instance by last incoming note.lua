0300eefdf81aa789898cebc78284f1aebf50106a
mpl <m.pilyavskiy@gmail.com>
2022-09-29 18:58:57 +0300

    2909

--- FX specific/mpl_RS5k tools/mpl_Float RS5k instance by last incoming note.lua
+++ FX specific/mpl_RS5k tools/mpl_Float RS5k instance by last incoming note.lua
@@ -1,13 +1,14 @@
 -- @description Float RS5k instance by last incoming note
--- @version 1.01
+-- @version 1.03
 -- @author MPL
 -- @website https://forum.cockos.com/showthread.php?t=188335
 -- @noindex
 -- @changelog
---    + Catch record armed track if not found on selected track
+--    # fix math for detecting rs5k note
+--    + Select founded track
 
 
-  local vrs = 'v1.02'
+  local vrs = 'v1.03'
  --------------------------------------------------------------------
   function main_sub(tr)
     if not tr then return end
@@ -47,8 +48,9 @@
       if buf =='Gain for minimum velocity' then -- validate fx is rs5k
         local nrangest = TrackFX_GetParamNormalized( track, fx-1, 3 ) -- note range start
         local nrangeendd = TrackFX_GetParamNormalized( track, fx-1, 4 ) -- note range end
-        if math.floor( nrangest *127) == base_pitch and  math.floor(nrangeendd *127) == base_pitch then 
+        if math.floor( nrangest *128) == base_pitch and  math.floor(nrangeendd *128) == base_pitch then 
           reaper.TrackFX_SetOpen( track, fx-1, true )
+          reaper.SetOnlyTrackSelected( track )
           return true
         end
       end
@@ -69,7 +71,7 @@
     end
   end
   ----------------------------------------------------------------------
-  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
+  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
   --------------------------------------------------------------------  
   local ret = VF_CheckFunctions(3.18) if ret then local ret2 = VF_CheckReaperVrs(6.64,true) if ret2 then 
      base_pitch = getlastnote()
