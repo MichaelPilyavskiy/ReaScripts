b1e9f3fcd423be80ccbea74b3921c8bfc52edc93
mpl <m.pilyavskiy@gmail.com>
2022-10-15 16:17:22 +0300

    another try for jsfx

--- FX specific/mpl_RS5k manager (background).lua
+++ FX specific/mpl_RS5k manager (background).lua
@@ -1,5 +1,5 @@
 -- @description RS5k manager
--- @version 3.0beta19
+-- @version 3.0beta20
 -- @author MPL
 -- @website https://forum.cockos.com/showthread.php?t=207971
 -- @about Script for handling ReaSamplomatic5000 data on group of connected tracks
@@ -179,7 +179,7 @@
   ---------------------------------------------------------------------  
   function main()  
     if not DATA.extstate then DATA.extstate = {} end
-    DATA.extstate.version = '3.0beta19'
+    DATA.extstate.version = '3.0beta20'
     DATA.extstate.extstatesection = 'MPL_RS5K manager'
     DATA.extstate.mb_title = 'RS5K manager'
     DATA.extstate.default = 
