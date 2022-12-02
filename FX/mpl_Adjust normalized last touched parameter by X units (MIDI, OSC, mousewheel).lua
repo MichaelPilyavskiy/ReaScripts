85520c5b22649af8b8e7dd5e2b3f89339062da59
mpl <m.pilyavskiy@gmail.com>
2022-12-02 23:43:40 +0300

    add takeFX support

--- FX/mpl_Adjust normalized last touched parameter by X units (MIDI, OSC, mousewheel).lua
+++ FX/mpl_Adjust normalized last touched parameter by X units (MIDI, OSC, mousewheel).lua
@@ -1,5 +1,5 @@
 -- @description Adjust normalized last touched parameter by X units (MIDI, OSC, mousewheel)
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @metapackage
@@ -8,21 +8,36 @@
 --    [main] . > mpl_Adjust normalized last touched parameter by 0.001 units (MIDI, OSC, mousewheel).lua
 --    [main] . > mpl_Adjust normalized last touched parameter by 0.0001 units (MIDI, OSC, mousewheel).lua
 -- @changelog
---    # change name
+--    + Add support for take FX
+
+
+
 
   local is_new_value,filename,sectionID,cmdID,mode,resolution,val = reaper.get_action_context()
-  
-  
   function mainsub(val, dir)
     if not dir then return end
      retval, trackid, fxid, paramid = reaper.GetLastTouchedFX()
     if retval then
+      local take_id = fxid>>16
+      local item_id = trackid>>16
+      local trackid = trackid&0xFFFF
+      local fxid = fxid&0xFFFF
       local track = reaper.GetTrack(0, trackid-1)
       if trackid == 0 then track = reaper.GetMasterTrack( 0 ) end
       if track then
-        local value0 = reaper.TrackFX_GetParamNormalized(track, fxid, paramid)
-        local newval = math.max(0,math.min(value0 + val*dir,math.huge))
-        reaper.TrackFX_SetParamNormalized(track, fxid, paramid, newval) 
+        
+        if item_id ~= 0 then
+          local item = reaper.GetTrackMediaItem( track, item_id-1 )
+          local take = reaper.GetTake( item, take_id ) 
+          local value0 = reaper.TakeFX_GetParamNormalized(take, fxid, paramid)
+          local newval = math.max(0,math.min(value0 + val*dir,math.huge))
+          reaper.TakeFX_SetParamNormalized(take, fxid, paramid, newval) 
+         else 
+          local value0 = reaper.TrackFX_GetParamNormalized(track, fxid, paramid)
+          local newval = math.max(0,math.min(value0 + val*dir,math.huge))
+          reaper.TrackFX_SetParamNormalized(track, fxid, paramid, newval) 
+        end
+        
       end  
     end
   end  
--- FX/mpl_Adjust normalized last touched parameter.lua
+++ FX/mpl_Adjust normalized last touched parameter.lua
@@ -1,5 +1,5 @@
 -- @description Adjust normalized last touched parameter
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @metapackage
@@ -11,35 +11,52 @@
 --    [main] . > mpl_Subtract 0.001 from normalized last touched parameter.lua
 --    [main] . > mpl_Subtract 0.0001 from normalized last touched parameter.lua
 -- @changelog
---    # proper parsing script name
+--    + Add support for take FX
+
  
   --NOT gfx NOT reaper
   function main(val, dir)
-    local dir_str if dir == 1 then dir_str = 'Add X to' else dir_str = 'Subtract X from' end
-    local scr_title = dir_str:gsub('X', val).." last touched FX parameter"
-     retval, trackid, fxid, paramid = GetLastTouchedFX()
+    if not dir then return end
+     retval, trackid, fxid, paramid = reaper.GetLastTouchedFX()
     if retval then
-      local track = GetTrack(0, trackid-1)
-      if trackid == 0 then track = GetMasterTrack( 0 ) end
+      local take_id = fxid>>16
+      local item_id = trackid>>16
+      local trackid = trackid&0xFFFF
+      local fxid = fxid&0xFFFF
+      local track = reaper.GetTrack(0, trackid-1)
+      if trackid == 0 then track = reaper.GetMasterTrack( 0 ) end
       if track then
-        Undo_BeginBlock()
-        local value0 = TrackFX_GetParamNormalized(track, fxid, paramid)
-        local newval = math.max(0,math.min(value0 + val*dir,math.huge))
-        TrackFX_SetParamNormalized(track, fxid, paramid, newval) 
-        Undo_EndBlock(scr_title, 1)
+        
+        if item_id ~= 0 then
+          local item = reaper.GetTrackMediaItem( track, item_id-1 )
+          local take = reaper.GetTake( item, take_id ) 
+          local value0 = reaper.TakeFX_GetParamNormalized(take, fxid, paramid)
+          local newval = math.max(0,math.min(value0 + val*dir,math.huge))
+          reaper.TakeFX_SetParamNormalized(take, fxid, paramid, newval) 
+         else 
+          local value0 = reaper.TrackFX_GetParamNormalized(track, fxid, paramid)
+          local newval = math.max(0,math.min(value0 + val*dir,math.huge))
+          reaper.TrackFX_SetParamNormalized(track, fxid, paramid, newval) 
+        end
+        
       end  
     end
   end  
----------------------------------------------------------------------
-  function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
--------------------------------------------------------------------- 
-  local val = ({reaper.get_action_context()})[2]:match('Add%s([%d%p]+%s)')
-  if not val then  val = ({reaper.get_action_context()})[2]:match('Subtract%s([%d%p]+%s)') end
-  val = val:match('[%d%p]+')
-  if not (val and tonumber(val)) then val = 0.01 else val = tonumber(val) end
-   dir = ({reaper.get_action_context()})[2]:match('Add')
-  if dir then dir = 1 else dir = -1 end
+  ----------------------------------------------------------------------
+  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
+  --------------------------------------------------------------------  
+  local ret = VF_CheckFunctions(3.51) if ret then local ret2 = VF_CheckReaperVrs(6.68,true) if ret2 then 
+    local val = ({reaper.get_action_context()})[2]:match('Add%s([%d%p]+%s)')
+    if not val then  val = ({reaper.get_action_context()})[2]:match('Subtract%s([%d%p]+%s)') end
+    val = val:match('[%d%p]+')
+    if not (val and tonumber(val)) then val = 0.01 else val = tonumber(val) end
+    dir = ({reaper.get_action_context()})[2]:match('Add')
+    if dir then dir = 1 else dir = -1 end
+    local dir_str if dir == 1 then dir_str = 'Add X to' else dir_str = 'Subtract X from' end
+    local scr_title = dir_str:gsub('X', val).." last touched FX parameter"
+    Undo_BeginBlock()
+    main(val, dir)
+    Undo_EndBlock(scr_title, 1)
+  end end
   
-  local ret = CheckFunctions('VF_CheckReaperVrs') 
-  local ret2 = VF_CheckReaperVrs(5.95)    
-  if ret and ret2 then main(val, dir) end
+  
