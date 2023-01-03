3409b31ec477889eda95b587f67b0feeb5cfc3a4
mpl <m.pilyavskiy@gmail.com>
2023-01-03 15:50:19 +0300

    0301

--- MIDI/mpl_Export selected items as MIDI files into project path.lua
+++ MIDI/mpl_Export selected items as MIDI files into project path.lua
@@ -1,10 +1,9 @@
--- @description Export project MIDI into project path
--- @version 1.0alpha4
+-- @description Export selected items as MIDI files into project path
+-- @version 1.0
 -- @author MPL
 -- @changelog
---    # fix IEEE 754 to float convertion
+--    + init
 
-  -- [[debug search filter: NOT function NOT reaper NOT gfx NOT VF]]
   
   DATA2 = {
     ppq_step = 1,
@@ -356,7 +355,5 @@
   function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
   --------------------------------------------------------------------  
   local ret = VF_CheckFunctions(3.42) if ret then local ret2 = VF_CheckReaperVrs(6.68,true) if ret2 then 
-    reaper.Undo_BeginBlock2( 0 )
     main() 
-    reaper.Undo_EndBlock2( 0, 'test', 0xFFFFFFFF )
   end end
