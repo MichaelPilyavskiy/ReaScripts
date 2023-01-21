8799f0edc25ab775393e4538a1bb3b35e70b4e66
mpl <m.pilyavskiy@gmail.com>
2023-01-21 20:46:10 +0300

    Update mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua

--- Tracks Properties/mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua
+++ Tracks Properties/mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua
@@ -1,19 +1,21 @@
--- @version 1.0
--- @author MPL
--- @changelog
---    + init  
 -- @description Toggle parent and regular send for selected tracks, ignore MIDI when regular
+-- @version 1.01
+-- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
+-- @changelog
+--    # set send mode to post fader
+--    + Add undo
   
   for key in pairs(reaper) do _G[key]=reaper[key]  end  
   function main(tr)
     if not tr then return end
     local mast_send = GetMediaTrackInfo_Value( tr, 'B_MAINSEND' )
-    local parent_tr = GetParentTrack( tr ) 
+    local parent_tr = GetParentTrack( tr )  
     if mast_send == 1 then 
       SetMediaTrackInfo_Value( tr, 'B_MAINSEND',0 )
       local s_id = CreateTrackSend( tr,  parent_tr)
       SetTrackSendInfo_Value( tr, 0, s_id, 'I_MIDIFLAGS',-1 )
+      SetTrackSendInfo_Value( tr, 0, s_id, 'I_SENDMODE',0 )
      else
       SetMediaTrackInfo_Value( tr, 'B_MAINSEND',1)
       for i = 1, GetTrackNumSends( tr, 0 ) do
@@ -26,7 +28,9 @@
     end
   end
   
+  reaper.Undo_BeginBlock2( 0 )
   for trid = 1, CountSelectedTracks(0) do
     tr= GetSelectedTrack(0,trid-1)
     main(tr)
   end
+  reaper.Undo_EndBlock2( 0, 'Toggle parent and regular send for selected tracks', 0xFFFFFFFF )
