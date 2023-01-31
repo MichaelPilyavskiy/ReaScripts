fa26003cbc5fe1fd434a2e831e3d9ae940b180de
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-01-31 22:33:15 +0300

    param mod actions

--- FX/mpl_Disable LFO for last touched parameter.lua
+++ FX/mpl_Disable LFO for last touched parameter.lua
@@ -1,10 +1,9 @@
--- @description Set auxilary input as a modulation for last touched parameter
--- @version 1.01
+-- @description Disable LFO for last touched parameter
+-- @version 1.0
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # use native API REAPER 6.73
---    # improve using GetLastTouchedFX
+--    + init
   
   ----------------------------------------------------------------
   function main()
@@ -18,11 +17,8 @@
      if trid==0 then tr = GetMasterTrack(0) else tr = GetTrack(0,trid-1) end
      if not tr then return end
      
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.active', 1)
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.chan', 2)
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.stereo', 1)
-     
-     --param.X.acs.[active,dir,strength,attack,release,dblo,dbhi,chan,stereo,x2,y2] : parameter modulation ACS state
+     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.lfo.active', 0)
+     --param.X.lfo.[active,dir,phase,speed,strength,temposync,free,shape] : parameter moduation LFO state
   end
   ----------------------------------------------------------------------
   function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
@@ -30,6 +26,6 @@
   local ret = VF_CheckFunctions(3.50) if ret then local ret2 = VF_CheckReaperVrs(6.37,true) if ret2 then  
     Undo_BeginBlock2( 0 )
     main()
-    Undo_EndBlock2( 0, 'Set auxilary input as a modulation for last touched parameter', 0xFFFFFFFF )
+    Undo_EndBlock2( 0, 'Disable LFO for last touched parameter', 0xFFFFFFFF )
   end end
   
--- FX/mpl_Remove OSC learn from selected tracks.lua
+++ FX/mpl_Remove OSC learn from selected tracks.lua
@@ -1,28 +1,23 @@
--- @description Set main input as a modulation for last touched parameter
--- @version 1.01
+-- @description Remove OSC learn from selected tracks
+-- @version 1.0
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # use native API REAPER 6.73
---    # improve using GetLastTouchedFX
+--    + init 
+
+  script_title_out = 'Remove OSC learn from selected tracks'
   
-  ----------------------------------------------------------------
   function main()
-     local retval, tracknumber, fxnumber, paramnumber = reaper.GetLastTouchedFX()
-     if not retval then return end
-     
-     local trid = tracknumber&0xFFFF
-     local itid = (tracknumber>>16)&0xFFFF
-     if itid > 0 then return end -- ignore item FX
-     local tr
-     if trid==0 then tr = GetMasterTrack(0) else tr = GetTrack(0,trid-1) end
-     if not tr then return end
-     
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.active', 1)
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.chan', 1)
-     TrackFX_SetNamedConfigParm( tr, fxnumber, 'param.'..paramnumber..'.acs.stereo', 1)
-     
-     --param.X.acs.[active,dir,strength,attack,release,dblo,dbhi,chan,stereo,x2,y2] : parameter modulation ACS state
+    for i = 1, CountSelectedTracks()do
+      local tr =GetSelectedTrack(0,i-1)
+      for fxnumber = 1,  TrackFX_GetCount( tr ) do
+        for p = 1,  TrackFX_GetNumParams( tr, fxnumber-1 ) do
+          --TrackFX_SetNamedConfigParm( tr, fxnumber-1, 'param.'..(p-1)..'.learn.midi1','' )
+          --TrackFX_SetNamedConfigParm( tr, fxnumber-1, 'param.'..(p-1)..'.learn.midi2','' )
+          TrackFX_SetNamedConfigParm( tr, fxnumber-1,  'param.'..(p-1)..'.learn.osc','' )
+        end
+      end
+    end
   end
   ----------------------------------------------------------------------
   function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
@@ -30,6 +25,5 @@
   local ret = VF_CheckFunctions(3.50) if ret then local ret2 = VF_CheckReaperVrs(6.37,true) if ret2 then  
     Undo_BeginBlock2( 0 )
     main()
-    Undo_EndBlock2( 0, 'Set main input as a modulation for last touched parameter', 0xFFFFFFFF )
+    Undo_EndBlock2( 0, script_title_out, 0xFFFFFFFF )
   end end
-  
