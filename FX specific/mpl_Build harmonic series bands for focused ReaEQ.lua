4b775249fbd15ed9127bddae1eda1598c2e7c64c
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-02-02 14:19:24 +0300

    Update mpl_Build harmonic series bands for focused ReaEQ.lua

--- FX specific/mpl_Build harmonic series bands for focused ReaEQ.lua
+++ FX specific/mpl_Build harmonic series bands for focused ReaEQ.lua
@@ -1,96 +1,61 @@
 -- @description Build harmonic series bands for focused ReaEQ
--- @version 1.05
+-- @version 1.06
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
+-- @noindex
 -- @changelog
---    # reindex out of metapackage by lot of requests
+--    # overhaul, code cleanup
+--    # using native API for parameter linking
+--    # remove all parameters except setting base frequency
+--    # change type to band
+--    + Improve BW link offset / scale
   
   
   
   -- NOT reaper NOT gfx
-  for key in pairs(reaper) do _G[key]=reaper[key]  end 
-  function msg(s) if s then ShowConsoleMsg(s..'\n') end end
   -----------------------------------------------------------------------------
-   function lim(val, min,max) --local min,max 
-     if not min or not max then min, max = 0,1 end 
-     return math.max(min,  math.min(val, max) ) 
-   end
-  -----------------------------------------------------------------------------
-  function MPL_BuildEQReduce(freq, gain, slope, linkBW, linkgain)
-    if not (freq and gain and slope) then return end
+  function MPL_BuildEQReduce(freq)
+    if not freq then return end
+    
+    -- handle reaEQ
     local  retval, tracknumber, itemnumber, fx = GetFocusedFX()
     if not (retval ==1 and fx >= 0) then return end
     local tr = CSurf_TrackFromID( tracknumber, false )
     local isReaEQ = TrackFX_GetEQParam( tr, fx, 0 )
     if not isReaEQ then return end
-    local num_params = TrackFX_GetNumParams( tr, fx )
-    local linkBW_str = ''
-    local linkgain_str = ''
-    local bands_cnt = math.floor((num_params - 2)/3)
-    for param = 1, num_params - 2, 3 do
-      local band_id = math.floor(param/3)
-      TrackFX_SetNamedConfigParm( tr, fx, 'BANDTYPE'..band_id, 2 )
-      local gain_new = lim(gain + band_id * slope, 0.01,0.99)
-      TrackFX_SetParamNormalized( tr, fx,  param,gain_new) -- -2.0dB gain
-      local startQdenom = 40
-      TrackFX_SetParamNormalized( tr, fx, param+1, lim(1/(startQdenom* (band_id+1)), 0.01, 1)) -- 0.5 Q
-      local ConvertedF = F2ReaEQVal(freq * (band_id+1) )
-      TrackFX_SetParamNormalized( tr, fx, param-1, ConvertedF) -- freq
-      if param > 1 then 
-      
-        if linkBW then 
-          linkBW_str = linkBW_str..'\n'..
-        -- Q
-[[      <PROGRAMENV ]]..(param+1)..[[ 0
-        PARAMBASE 0
-        LFO 0
-        LFOWT 1 1
-        AUDIOCTL 0
-        AUDIOCTLWT 1 1
-        PLINK 1 0:0 2 0
-        MODWND 0
-      >      
-]]        
-        end
-        
-        if linkgain then
-          local scale = 1
-          local offset = 0
-          linkBW_str = linkBW_str..'\n'..
-        -- Q
-[[      <PROGRAMENV ]]..(param)..[[ 0
-        PARAMBASE 0
-        LFO 0
-        LFOWT 1 1
-        AUDIOCTL 0
-        AUDIOCTLWT 1 1
-        PLINK 1 0:0 ]]..scale..' '..offset..[[ 
-        MODWND 0
-      >      
-]]   
-      end
-      end
-    end
     
-    local out_chunk
-    if linkBW or linkgain then 
-      local out_chunk = Link(tr, fx, linkBW_str) -- link bandQ
-      SetTrackStateChunk( tr, out_chunk, true ) 
-    end
-  end
-  ---------------------------------------------------
-  function literalize(str) -- http://stackoverflow.com/questions/1745448/lua-plain-string-gsub
-     if str then  return str:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", function(c) return "%" .. c end) end
-  end
-  -----------------------------------------------------------------------------
-  function Link(tr, fx, build_str, chunk) local chunk
-    if not chunk then 
-      retval, chunk = GetTrackStateChunk( tr, '', false )
-    end
-    local fxGUID = TrackFX_GetFXGUID( tr, fx )
-    --local build_str = ''
-    local chunk = chunk:gsub(literalize(fxGUID)..'.-WAK', fxGUID..build_str..'WAK')
-    return chunk
+    -- init params
+      TrackFX_SetParamNormalized( tr, fx, 0, F2ReaEQVal(freq)) -- F
+      TrackFX_SetParamNormalized( tr, fx, 1, 0.25) -- G
+      TrackFX_SetParamNormalized( tr, fx, 2, .01) -- Q
+      
+    -- cnt bands
+      local bands_cnt=0
+      for band_id = 0, 50 do
+        local ret,str = TrackFX_GetNamedConfigParm( tr, fx, 'BANDTYPE'..band_id )
+        if not ret then break end
+        bands_cnt = bands_cnt + 1
+      end
+    -- set bands  
+      for band_id = 1, bands_cnt do
+        -- freq
+        local Fid = band_id*3 + 0
+        TrackFX_SetParamNormalized( tr, fx, Fid, F2ReaEQVal(freq * (band_id+1) )) 
+        -- G
+        local Gid = band_id*3 + 1
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.mod.active', 1 )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.plink.active', 1 )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.plink.effect', fx )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.plink.param', 1 )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.plink.scale', 1- ( band_id / bands_cnt) )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Gid..'.mod.baseline', 0.25* ( band_id / bands_cnt) )
+        -- Q
+        local Qid = band_id*3 + 2
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Qid..'.mod.active', 1 )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Qid..'.plink.active', 1 )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Qid..'.plink.effect', fx )
+        TrackFX_SetNamedConfigParm( tr, fx, 'param.'..Qid..'.plink.param', 2 )
+      end
   end
   ----------------------------------------------------------------------------- 
   function ReaEQVal2F(x) 
@@ -116,22 +81,24 @@
     local curve =  (F - 20) / (24000 - 20)
     local x = (math.log((curve +0.0025) / 0.0025,math.exp(1) ))/math.log(401)
     return x
-  end
-  ----------------------------------------------------------------------------- 
-  local def_params = '50,-150,0.01,Y,N'
-  local extstate = GetExtState('MPL_BUILDHARMSERREAEQ', 'params')
-  if extstate ~= '' then def_params = extstate end
-  local retval, str = GetUserInputs('Build harmonic bands', 5, 'Base frequency(Hz) (def=50),Gain Reduction(dB) (def=-150),Gain increment/decrement (dB),Link banwidth (Y/N),Link gain (Y/N)', def_params)
-  if retval then 
-    local t, freq, gain, slope, linkbands, linkgain = {}, _, 0, 0.01, true, false
-    for val in str:gmatch('[^,]+') do  t[#t+1] = val end
-    if t[1] then freq = t[1] else return end
-    if t[2] then gain = ParseDbVol(t[2]) end
-    if t[3] then slope = t[3] end
-    if t[4] then linkbands = t[4]:lower() == 'y' end
-    if t[5] then linkgain = t[5]:lower() == 'y' end
-    if freq and gain and slope then 
-      SetExtState( 'MPL_BUILDHARMSERREAEQ', 'params', table.concat(t, ','), true )
+  end 
+  ----------------------------------------------------------------------
+  function main()
+    local retval, freq = GetUserInputs('Build harmonic bands', 1, 'Base frequency (Hz)', 50)
+    if retval and freq ~= '' then 
+      freq = freq:match('[%d%p]+')
+      if not freq then return end
+      freq = tonumber(freq)
+      if not freq then return end
+      MPL_BuildEQReduce(freq)
     end
-    MPL_BuildEQReduce(freq, lim(gain, 0,1), slope, linkbands, linkgain)
   end
+  ----------------------------------------------------------------------
+  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
+  --------------------------------------------------------------------  
+  local ret = VF_CheckFunctions(3.15) if ret then local ret2 = VF_CheckReaperVrs(6.37,true) if ret2 then 
+    Undo_BeginBlock2( 0 )
+    main() 
+    Undo_EndBlock2( 0, 'Build harmonic series bands for focused ReaEQ', 0xFFFFFFFF )
+  end end
+  
