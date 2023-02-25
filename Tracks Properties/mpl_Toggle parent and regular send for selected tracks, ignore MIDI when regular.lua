68faa9fb3e1deb7824b66c2e182d7e0d2839ee4f
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-02-25 12:37:19 +0300

    Update mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua

--- Tracks Properties/mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua
+++ Tracks Properties/mpl_Toggle parent and regular send for selected tracks, ignore MIDI when regular.lua
@@ -1,31 +1,38 @@
 -- @description Toggle parent and regular send for selected tracks, ignore MIDI when regular
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # set send mode to post fader
---    + Add undo
+--    + Hardcode send level to 0dB
+--    # remove SWS dependency
+--    # ignore track if parent is master
   
   for key in pairs(reaper) do _G[key]=reaper[key]  end  
   function main(tr)
     if not tr then return end
     local mast_send = GetMediaTrackInfo_Value( tr, 'B_MAINSEND' )
-    local parent_tr = GetParentTrack( tr )  
+    local parent_tr = GetParentTrack( tr )   
+    local parent_trID = CSurf_TrackToID(parent_tr, false) 
+    if not parent_trID then return end
+    
     if mast_send == 1 then 
-      SetMediaTrackInfo_Value( tr, 'B_MAINSEND',0 )
+      SetMediaTrackInfo_Value( tr, 'B_MAINSEND',0 ) 
       local s_id = CreateTrackSend( tr,  parent_tr)
       SetTrackSendInfo_Value( tr, 0, s_id, 'I_MIDIFLAGS',-1 )
       SetTrackSendInfo_Value( tr, 0, s_id, 'I_SENDMODE',0 )
+      SetTrackSendInfo_Value( tr, 0, s_id, 'D_VOL',1 ) -- overwrite send level to 0 dB
      else
       SetMediaTrackInfo_Value( tr, 'B_MAINSEND',1)
       for i = 1, GetTrackNumSends( tr, 0 ) do
-        local dest_tr = BR_GetMediaTrackSendInfo_Track( tr, 0, i-1, 1 )
+        local dest_tr = GetTrackSendInfo_Value(tr, 0, i-1, 'P_DESTTRACK')
         if dest_tr == parent_tr then
           RemoveTrackSend( tr, 0, i-1 )
           break
         end
       end
     end
+    
+    
   end
   
   reaper.Undo_BeginBlock2( 0 )
