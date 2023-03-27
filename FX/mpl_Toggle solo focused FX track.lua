20b6e76dbf0014762607794b5c62375bf6e13479
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-03-27 13:53:11 +0300

    Update mpl_Toggle solo focused FX track.lua

--- FX/mpl_Toggle solo focused FX track.lua
+++ FX/mpl_Toggle solo focused FX track.lua
@@ -1,9 +1,9 @@
 -- @description Toggle solo focused FX track
--- @version 1.0
+-- @version 1.01
 -- @author MPL
 -- @website http://forum.cockos.com/member.php?u=70694
 -- @changelog
---   + init
+--   # Use solo in place
   
   reaper.Undo_BeginBlock()
   local _, tracknumber = reaper.GetFocusedFX()
@@ -11,7 +11,15 @@
   if tracknumber >= 0 then 
     local tr =  reaper.CSurf_TrackFromID( tracknumber, false )
     local solo_state = reaper.GetMediaTrackInfo_Value( tr, param )
-    reaper.SetMediaTrackInfo_Value( tr, param, math.abs(solo_state-1) )
+    local recarmstate = 0
+    if solo_state > 0 then 
+      solo_state = 0 
+     else 
+      solo_state = 2 
+      recarmstate = 1
+    end
+    reaper.SetMediaTrackInfo_Value( tr, param, solo_state )
+    reaper.SetMediaTrackInfo_Value( tr, 'I_RECARM', recarmstate )
     reaper.TrackList_AdjustWindows( false )
   end
   reaper.Undo_EndBlock('Toggle solo focused FX track', 0 )
