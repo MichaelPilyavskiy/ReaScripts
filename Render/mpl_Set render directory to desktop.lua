60efdc164c60d5c1586d5580f465683abc31ea53
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-07-31 20:12:55 +0300

    Update mpl_Set render directory to desktop.lua

--- Render/mpl_Set render directory to desktop.lua
+++ Render/mpl_Set render directory to desktop.lua
@@ -1,24 +1,33 @@
 -- @description Set render directory to desktop
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
--- @provides [windows] .
 -- @changelog
---    # change header
+--    # fix error on MacOS
+--    + Add MacOS support
+
  
   function GetDesktopPath() --https://forums.coronalabs.com/topic/59382-system-directories-on-windows-builds/
-    local desktopPath = os.getenv("appdata")
-    local appDataStart = string.find( desktopPath, "AppData" )
-    if( appDataStart ) then
-       desktopPath = string.sub( desktopPath, 1, appDataStart-1 )
-       desktopPath = desktopPath .. "Desktop\\"
+    if GetOS():lower():match('win') then 
+      local desktopPath = os.getenv("appdata")
+      local appDataStart = string.find( desktopPath, "AppData" )
+      if( appDataStart ) then
+         desktopPath = string.sub( desktopPath, 1, appDataStart-1 )
+         desktopPath = desktopPath .. "Desktop\\"
+      end
+      return true, desktopPath
     end
-    return true, desktopPath
+    
+    if GetOS():lower():match('mac') then 
+      local desktopPath = os.getenv("HOME")..'/Desktop'
+      return true, desktopPath
+    end
+    
   end
   -------------------
   function main()
     local ret, desktopPath = GetDesktopPath()
-    if ret and GetOS():match('Win') then reaper.GetSetProjectInfo_String(0, 'RENDER_FILE', desktopPath, true) end
+    if ret then reaper.GetSetProjectInfo_String(0, 'RENDER_FILE', desktopPath, true) end
   end  
 ---------------------------------------------------------------------
   function CheckFunctions(str_func) local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua' local f = io.open(SEfunc_path, 'r')  if f then f:close() dofile(SEfunc_path) if not _G[str_func] then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to newer version', '', 0) else return true end  else reaper.MB(SEfunc_path:gsub('%\\', '/')..' missing', '', 0) end   end
