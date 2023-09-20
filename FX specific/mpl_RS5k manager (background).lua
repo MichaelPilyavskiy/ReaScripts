85e5201fdf4cd5ab98018c125f72a641067787fd
mpl <12525837+MichaelPilyavskiy@users.noreply.github.com>
2023-09-20 22:55:54 +0300

    Update mpl_RS5k manager (background).lua

--- FX specific/mpl_RS5k manager (background).lua
+++ FX specific/mpl_RS5k manager (background).lua
@@ -1,5 +1,5 @@
 -- @description RS5k manager
--- @version 3.21
+-- @version 3.22
 -- @author MPL
 -- @website https://forum.cockos.com/showthread.php?t=207971
 -- @about Script for handling ReaSamplomatic5000 data on group of connected tracks
@@ -16,7 +16,7 @@
 --    mpl_RS5k_manager_MacroControls.jsfx 
 --    mpl_RS5K_manager_MIDIBUS_choke.jsfx
 -- @changelog
---    # DrumRack: replacing sample obey Settings/On sample add/Rename track
+--    + Macro: add attack links
 
 
 
@@ -30,7 +30,7 @@
   ---------------------------------------------------------------------  
   function main()  
     if not DATA.extstate then DATA.extstate = {} end
-    DATA.extstate.version = '3.21'
+    DATA.extstate.version = '3.22'
     DATA.extstate.extstatesection = 'MPL_RS5K manager'
     DATA.extstate.mb_title = 'RS5K manager'
     DATA.extstate.default = 
@@ -2479,11 +2479,11 @@ rightclick them to hide all but active.
                     DATA2:ProcessUndoBlock(f, 'RS5k manager / Macro / Add tune links',macroID) 
                   end
           },
+          
           {str= 'Add selected/all RS5k samplers gain, obey_offsets',
            func = function() 
                     local cnt_selection = 0 
-                    if DATA2.PADselection then for keynote in pairs(DATA2.PADselection) do if DATA2.PADselection[keynote] then cnt_selection = cnt_selection + 1 end end end
-                    
+                    if DATA2.PADselection then for keynote in pairs(DATA2.PADselection) do if DATA2.PADselection[keynote] then cnt_selection = cnt_selection + 1 end end end 
                     local f = function(macroID)
                       local macro_t = DATA2.Macro.sliders[macroID]
                       local notest = {}
@@ -2512,7 +2512,42 @@ rightclick them to hide all but active.
                     end
                     DATA2:ProcessUndoBlock(f, 'RS5k manager / Macro / Add gain links',macroID) 
                   end
-          },          
+          },      
+          
+          {str= 'Add selected/all RS5k samplers attack, obey_offsets',
+           func = function() 
+                    local cnt_selection = 0 
+                    if DATA2.PADselection then for keynote in pairs(DATA2.PADselection) do if DATA2.PADselection[keynote] then cnt_selection = cnt_selection + 1 end end end
+                    
+                    local f = function(macroID)
+                      local macro_t = DATA2.Macro.sliders[macroID]
+                      local notest = {}
+                      if cnt_selection == 0 then 
+                        for note in pairs(DATA2.notes) do notest[#notest+1] = note end -- all
+                       else
+                        for keynote in pairs(DATA2.PADselection) do notest[#notest+1] = keynote end -- selected only
+                      end
+                      
+                      for notestID = 1, #notest do
+                        local note = notest[notestID]
+                        if DATA2.notes[note] and DATA2.notes[note].layers then 
+                          for layer in pairs(DATA2.notes[note].layers) do
+                            local srct = DATA2.notes[note].layers[layer]
+                            if srct.ISRS5K then
+                              local instrpos = srct.instrument_pos
+                              local initparam = srct.instrument_vol
+                              local scale = 0.06
+                              local offs = initparam-0.5
+                              DATA2:Actions_Macro_AddLink(srct,instrpos,9, offs,scale)
+                            end
+                          end
+                        end
+                      end
+                      TrackFX_SetParamNormalized( macro_t.tr_ptr, macro_t.macro_pos, macroID, 0.5 )
+                    end
+                    DATA2:ProcessUndoBlock(f, 'RS5k manager / Macro / Add attack links',macroID) 
+                  end
+          },              
           {str= 'Clear links',
            func = function() 
                     local f = function(macroID) 
