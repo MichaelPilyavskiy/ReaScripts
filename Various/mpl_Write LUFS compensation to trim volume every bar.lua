06ebc1415e8f0dd0f3301fa27be3f4681637e647
MPL <m.pilyavskiy@gmail.com>
2024-09-01 14:38:14 +0300

    Update mpl_Write LUFS compensation to trim volume every bar.lua

--- Various/mpl_Write LUFS compensation to trim volume every bar.lua
+++ Various/mpl_Write LUFS compensation to trim volume every bar.lua
@@ -1,67 +1,49 @@
 -- @description Write LUFS compensation to trim volume every bar (background)
--- @version 1.0
+-- @version 1.01
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    + Init
+--    # ReaImGui based UI
+--    # VF independent
 
-
- 
-  -- NOT gfx NOT reaper NOT VF NOT GUI NOT DATA NOT MAIN 
+--------------------------------------------------------------------------------  init globals
+  for key in pairs(reaper) do _G[key]=reaper[key] end
+  app_vrs = tonumber(GetAppVersion():match('[%d%.]+'))
+  if app_vrs < 7 then return reaper.MB('This script require REAPER 7.0+','',0) end
+  local ImGui
   
-  -- config defaults
-  DATA2 = {state = false,
-          trigger_mintime= 0.5}
-  ---------------------------------------------------------------------  
-  function main()  
-    if not DATA.extstate then DATA.extstate = {} end
-    DATA.extstate.version = '1.0'
-    DATA.extstate.extstatesection = 'mpllufscomp'
-    DATA.extstate.mb_title = 'Write LUFS compensation'
-    DATA.extstate.default = 
-                          {  
-                          wind_x =  100,
-                          wind_y =  100,
-                          wind_w =  350,
-                          wind_h =  50,
-                          dock =    0,
-                          
-                          CONF_NAME = 'default',
-                          
-                          -- UI
-                          UI_appatchange = 0, 
-                          UI_enableshortcuts = 0,
-                          UI_initatmouse = 0,
-                          UI_showtooltips = 1,
-                          UI_groupflags = 0, 
-                          
-                          }
-    
-    DATA:ExtStateGet()
-    DATA:ExtStateGetPresets()  
-    if DATA.extstate.UI_initatmouse&1==1 then
-      local w = DATA.extstate.wind_w
-      local h = DATA.extstate.wind_h 
-      local x, y = GetMousePosition()
-      DATA.extstate.wind_x = x-w/2
-      DATA.extstate.wind_y = y-h/2
-    end
-    DATA:GUIinit()
-    
-    RUN()
-  end
+  if not reaper.ImGui_GetBuiltinPath then return reaper.MB('This script require ReaImGui extension','',0) end
+  package.path =   reaper.ImGui_GetBuiltinPath() .. '/?.lua'
+  ImGui = require 'imgui' '0.9.2'
+  
+  
+  
+  DATA = {  state = false,
+            ES_key = 'mpllufscomp',
+            UI_name = 'Write LUFS compensation',
+            trigger_mintime = 0.5,
+          }
+  EXT = {
+            viewport_posX = 100,
+            viewport_posY = 100,
+            viewport_posW_fixed = 200,
+            viewport_posH_fixed = 100, 
+          
+        }
+          
+  UI =  {
+  -- font  
+            font='Arial',
+            font1sz=15,
+            
+        }
+          
+          
+
+          
   ----------------------------------------------------------------------
-  function DATA2:PerformStuff(mode)
-    for i = 1, CountSelectedTracks(0)do
-      local tr = GetSelectedTrack(0,i-1)
-      if mode == 1 then DATA2:PerformStuff_atclose(tr)
-       elseif mode == 0 then DATA2:PerformStuff_atrun(tr)
-       elseif mode == 2 then DATA2:PerformStuff_atstart(tr)
-      end
-    end
-  end
-  ----------------------------------------------------------------------
-  function DATA2:PerformStuff_atstart(tr)
+  function DATA.PerformStuff_atstart(tr)
+    if not tr then return end
     local vu = GetMediaTrackInfo_Value( tr, 'I_VUMODE' )
     if vu ~= 16 then SetMediaTrackInfo_Value( tr, 'I_VUMODE', 16 ) end
     
@@ -96,9 +78,209 @@
     end
     
   end
-  ----------------------------------------------------------------------
-  function DATA2:PerformStuff_atrun(tr)
+  function DATA:handleProjUpdates()
+    local SCC =  GetProjectStateChangeCount( 0 ) if (DATA.upd_lastSCC and DATA.upd_lastSCC~=SCC ) then DATA.upd = true end  DATA.upd_lastSCC = SCC
+    local editcurpos =  GetCursorPosition()  if (DATA.upd_last_editcurpos and DATA.upd_last_editcurpos~=editcurpos ) then DATA.upd = true end DATA.upd_last_editcurpos=editcurpos 
+    local reaproj = tostring(EnumProjects( -1 )) if (DATA.upd_last_reaproj and DATA.upd_last_reaproj ~= reaproj) then DATA.upd = true end DATA.upd_last_reaproj = reaproj
+  end
+  -------------------------------------------------------------------------------- 
+  function UI.MAIN()
     
+    EXT:load() 
+    EXT.presetview = 0
+    
+    -- imgUI init
+    ctx = ImGui.CreateContext(DATA.UI_name) 
+    -- fonts
+    DATA.font1 = ImGui.CreateFont(UI.font, UI.font1sz) ImGui.Attach(ctx, DATA.font1)
+    -- config
+    --ImGui.SetConfigVar(ctx, ImGui.ConfigVar_HoverDelayNormal, UI.hoverdelay)
+    --ImGui.SetConfigVar(ctx, ImGui.ConfigVar_HoverDelayShort, UI.hoverdelayshort)
+    
+    
+    -- run loop
+    defer(UI.MAINloop)
+  end
+  -------------------------------------------------------------------------------- 
+  function DATA:handleViewportXYWH()
+    if not (DATA.display_x and DATA.display_y) then return end 
+    if not DATA.display_x_last then DATA.display_x_last = DATA.display_x end
+    if not DATA.display_y_last then DATA.display_y_last = DATA.display_y end
+    if not DATA.display_w_last then DATA.display_w_last = DATA.display_w end
+    if not DATA.display_h_last then DATA.display_h_last = DATA.display_h end
+    
+    if  DATA.display_x_last~= DATA.display_x 
+      or DATA.display_y_last~= DATA.display_y 
+      or DATA.display_w_last~= DATA.display_w 
+      or DATA.display_h_last~= DATA.display_h 
+      then 
+      DATA.display_schedule_save = os.clock() 
+    end
+    if DATA.display_schedule_save and os.clock() - DATA.display_schedule_save > 0.3 then 
+      EXT.viewport_posX = DATA.display_x
+      EXT.viewport_posY = DATA.display_y
+      EXT.viewport_posW = DATA.display_w
+      EXT.viewport_posH = DATA.display_h
+      EXT:save() 
+      DATA.display_schedule_save = nil 
+    end
+    DATA.display_x_last = DATA.display_x
+    DATA.display_y_last = DATA.display_y
+    DATA.display_w_last = DATA.display_w
+    DATA.display_h_last = DATA.display_h
+  end
+  -------------------------------------------------------------------------------- 
+  function EXT:load() 
+    if not DATA.ES_key then return end
+    for key in pairs(EXT) do 
+      if (type(EXT[key]) == 'string' or type(EXT[key]) == 'number') then 
+        if HasExtState( DATA.ES_key, key ) then 
+          local val = GetExtState( DATA.ES_key, key ) 
+          EXT[key] = tonumber(val) or val 
+        end 
+      end  
+    end 
+    DATA.upd = true
+  end
+  -------------------------------------------------------------------------------- 
+  function EXT:save() 
+    if not DATA.ES_key then return end 
+    for key in pairs(EXT) do 
+      if (type(EXT[key]) == 'string' or type(EXT[key]) == 'number') then 
+        SetExtState( DATA.ES_key, key, EXT[key], true  ) 
+      end 
+    end 
+    EXT:load()
+  end
+  -------------------------------------------------------------------------------- 
+  function UI.MAIN_draw(open) 
+    -- window_flags
+      local window_flags = ImGui.WindowFlags_None
+      --window_flags = window_flags | ImGui.WindowFlags_NoTitleBar
+      window_flags = window_flags | ImGui.WindowFlags_NoScrollbar
+      --window_flags = window_flags | ImGui.WindowFlags_MenuBar()
+      --window_flags = window_flags | ImGui.WindowFlags_NoMove()
+      window_flags = window_flags | ImGui.WindowFlags_NoResize
+      window_flags = window_flags | ImGui.WindowFlags_NoCollapse
+      --window_flags = window_flags | ImGui.WindowFlags_NoNav()
+      --window_flags = window_flags | ImGui.WindowFlags_NoBackground()
+      window_flags = window_flags | ImGui.WindowFlags_NoDocking
+      window_flags = window_flags | ImGui.WindowFlags_TopMost
+      window_flags = window_flags | ImGui.WindowFlags_NoScrollWithMouse
+      --if UI.disable_save_window_pos == true then window_flags = window_flags | ImGui.WindowFlags_NoSavedSettings() end
+      --window_flags = window_flags | ImGui.WindowFlags_UnsavedDocument()
+      --open = false -- disable the close button
+    
+    
+
+      
+    -- We specify a default position/size in case there's no data in the .ini file.
+      local main_viewport = ImGui.GetMainViewport(ctx)
+      local x, y, w, h =EXT.viewport_posX,EXT.viewport_posY, EXT.viewport_posW,EXT.viewport_posH
+      ImGui.SetNextWindowPos(ctx, x, y, ImGui.Cond_Appearing )
+      ImGui.SetNextWindowSize(ctx, EXT.viewport_posW_fixed,EXT.viewport_posH_fixed, ImGui.Cond_Always)
+      --ImGui.SetNextWindowSize(ctx, w_min, h_min, ImGui.Cond_Always)
+      
+      
+    -- init UI 
+      ImGui.PushFont(ctx, DATA.font1) 
+      local rv,open = ImGui.Begin(ctx, DATA.UI_name, open, window_flags) 
+      if rv then
+        local Viewport = ImGui.GetWindowViewport(ctx)
+        DATA.display_x, DATA.display_y = ImGui.Viewport_GetPos(Viewport) 
+        DATA.display_w, DATA.display_h = ImGui.Viewport_GetSize(Viewport) 
+        DATA.display_w_region, DATA.display_h_region = ImGui.Viewport_GetSize(Viewport) 
+        
+      -- calc stuff for childs
+        UI.calc_xoffset,UI.calc_yoffset = ImGui.GetStyleVar(ctx, ImGui.StyleVar_WindowPadding)
+        local framew,frameh = ImGui.GetStyleVar(ctx, ImGui.StyleVar_FramePadding)
+        local calcitemw, calcitemh = ImGui.CalcTextSize(ctx, 'test')
+        UI.calc_itemH = calcitemh + frameh * 2
+        UI.calc_itemH_small = math.floor(UI.calc_itemH*0.8)
+        
+      -- draw stuff
+        UI.draw()
+        ImGui.Dummy(ctx,0,0) 
+        ImGui.End(ctx)
+      end 
+      ImGui.PopFont( ctx ) 
+      if  ImGui.IsKeyPressed( ctx, ImGui.Key_Escape,false )  then return end
+    
+      return open
+  end
+  
+  --------------------------------------------------------------------------------  
+  function UI.draw()  
+    local inittxt = '[stopped]'
+    if DATA.state == true then inittxt = '[recording]' end
+    if ImGui.Button(ctx, inittxt,DATA.display_w_region-UI.calc_xoffset*2,DATA.display_h_region-UI.calc_yoffset*2-UI.calc_itemH) then   
+      DATA.state = not DATA.state 
+      if DATA.state == true then 
+        reaper.Undo_BeginBlock2(0 )
+        DATA.PerformStuff(2)
+        Undo_EndBlock2( 0, 'LUFS compensation', 0xFFFFFFFF )
+       else
+        DATA.PerformStuff(1)
+      end 
+    end 
+  end
+  ----------------------------------------------------------------------------------------- 
+  function main() 
+    UI.MAIN() 
+  end  
+  ----------------------------------------------------------------------
+  function DATA.DYNUPDATE()
+    -- stop if not active / playing
+      if not DATA.state then return end
+      if GetPlayStateEx( 0 )&1~= 1 then return end
+      
+    -- handle at beats
+      DATA.playpos = GetPlayPosition2Ex( 0 )
+      local beats, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( 0, DATA.playpos )
+      if beats > 0.2 then return end
+      
+    -- handle minimum time between triggers
+      DATA.trigger = os.clock()
+      if DATA.lasttrigger and DATA.trigger - DATA.lasttrigger < DATA.trigger_mintime then return end
+      DATA.lasttrigger = DATA.trigger
+      
+    -- perform stuff
+      DATA.PerformStuff(0)
+      
+  end
+    -------------------------------------------------------------------------------- 
+  function DATA.PerformStuff(mode)
+  
+    for i = 1, CountSelectedTracks(0)do
+      local tr = GetSelectedTrack(0,i-1)
+      if tr and reaper.ValidatePtr2(0,tr,'MediaTrack*') then 
+        if mode == 1 then DATA.PerformStuff_atclose(tr)
+         elseif mode == 0 then DATA.PerformStuff_atrun(tr)
+         elseif mode == 2 then DATA.PerformStuff_atstart(tr)
+        end
+      end
+    end
+  end
+    ------------------------------------------------------------------------------------------------------
+  function WDL_DB2VAL(x) return math.exp((x)*0.11512925464970228420089957273422) end  --https://github.com/majek/wdl/blob/master/WDL/db2val.h
+  ------------------------------------------------------------------------------------------------------
+  function WDL_VAL2DB(x, reduce)   --https://github.com/majek/wdl/blob/master/WDL/db2val.h
+    if not x or x < 0.0000000298023223876953125 then return -150.0 end
+    local v=math.log(x)*8.6858896380650365530225783783321
+    if v<-150.0 then return -150.0 else 
+      if reduce then 
+        return string.format('%.2f', v)
+       else 
+        return v 
+      end
+    end
+  end
+  ----
+  ----------------------------------------------------------------------
+  function DATA.PerformStuff_atrun(tr)
+  
+  
+  
     local env = GetTrackEnvelopeByName( tr, 'Trim Volume' )
     if not env then return end
     local scaling_mode = GetEnvelopeScalingMode( env )
@@ -118,7 +300,7 @@
     lufsout = math.min(lufsout, lufsout_max)
     lufsout = ScaleToEnvelopeMode( scaling_mode, lufsout )
     
-    local beats, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( 0, DATA2.playpos )
+    local beats, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( 0, DATA.playpos )
     local outpos =  TimeMap2_beatsToTime( 0, math.floor(fullbeats))
     local closepointID = GetEnvelopePointByTime( env, outpos+time_diff_allowed )
     local retval, time, value, shape, tension, selected = reaper.GetEnvelopePoint( env, closepointID )
@@ -130,56 +312,28 @@
     
   end  
   ----------------------------------------------------------------------
-  function DATA2:PerformStuff_atclose(tr)
-    SetMediaTrackInfo_Value( tr, 'I_VUMODE', 0 )
+  function DATA.PerformStuff_atclose(tr) 
+    SetMediaTrackInfo_Value( tr, 'I_VUMODE', 0 ) 
   end
-  ----------------------------------------------------------------------
-  function DATA_RESERVED_DYNUPDATE()
-    -- stop if not active / playing
-      if not DATA2.state then return end
-      if GetPlayStateEx( 0 )&1~= 1 then return end
-      
-    -- handle at beats
-      DATA2.playpos = GetPlayPosition2Ex( 0 )
-      local beats, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( 0, DATA2.playpos )
-      if beats > 0.2 then return end
-      
-    -- handle minimum time between triggers
-      DATA2.trigger = os.clock()
-      if DATA2.lasttrigger and DATA2.trigger - DATA2.lasttrigger < DATA2.trigger_mintime then return end
-      DATA2.lasttrigger = DATA2.trigger
-      
-    -- perform stuff
-      DATA2:PerformStuff(0)
-      
+  -------------------------------------------------------------------------------- 
+  function UI.MAINloop() 
+    DATA.clock = os.clock() 
+    DATA:handleProjUpdates()
+    DATA.flicker = math.abs(-1+(math.cos(math.pi*(DATA.clock%2)) + 1))
+    
+    DATA.DYNUPDATE()
+    --if DATA.upd == true then  DATA:CollectData()  end 
+    DATA.upd = false
+    
+    -- draw UI
+    UI.open = UI.MAIN_draw(true) 
+    
+    -- handle xy
+    DATA:handleViewportXYWH()
+    -- data
+    if UI.open then defer(UI.MAINloop) end
   end
-  --------------------------------------------------------------------- 
-  function GUI_RESERVED_init(DATA)
-    DATA.GUI.buttons = {}
-    local inittxt = '[stopped]'
-    if DATA2.state == true then inittxt = '[recording]' end
-    DATA.GUI.buttons.scroll = { x=0,
-                          y=0,
-                          w=gfx.w/DATA.GUI.default_scale-1,
-                          h=gfx.h/DATA.GUI.default_scale-1,
-                          txt = inittxt,
-                          onmouserelease = function() 
-                            DATA2.state = not DATA2.state 
-                            if DATA2.state == true then 
-                              DATA.GUI.buttons.scroll.txt = '[recording]'
-                              reaper.Undo_BeginBlock2(0 )
-                              DATA2:PerformStuff(2)
-                              Undo_EndBlock2( 0, 'LUFS compensation', 0xFFFFFFFF )
-                             else
-                              DATA.GUI.buttons.scroll.txt = '[stopped]'
-                              DATA2:PerformStuff(1)
-                            end
-                          end
-                          }         
-    for but in pairs(DATA.GUI.buttons) do DATA.GUI.buttons[but].key = but end
-  end 
-  ----------------------------------------------------------------------
-  function VF_CheckFunctions(vrs)  local SEfunc_path = reaper.GetResourcePath()..'/Scripts/MPL Scripts/Functions/mpl_Various_functions.lua'  if  reaper.file_exists( SEfunc_path ) then dofile(SEfunc_path)  if not VF_version or VF_version < vrs then  reaper.MB('Update '..SEfunc_path:gsub('%\\', '/')..' to version '..vrs..' or newer', '', 0) else return true end   else  reaper.MB(SEfunc_path:gsub('%\\', '/')..' not found. You should have ReaPack installed. Right click on ReaPack package and click Install, then click Apply', '', 0) if reaper.APIExists('ReaPack_BrowsePackages') then reaper.ReaPack_BrowsePackages( 'Various functions' ) else reaper.MB('ReaPack extension not found', '', 0) end end end
-  --------------------------------------------------------------------  
-  local ret = VF_CheckFunctions(3.42) if ret then local ret2 = VF_CheckReaperVrs(6.68,true) if ret2 then main() end end
+  
+  -----------------------------------------------------------------------------------------
+  main()
 
