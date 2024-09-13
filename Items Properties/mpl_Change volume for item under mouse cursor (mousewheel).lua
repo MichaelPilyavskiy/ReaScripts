2e354863cf6b76bf5bf2cb98a0657a6eda558686
MPL <m.pilyavskiy@gmail.com>
2024-09-13 23:12:56 +0300

    Update mpl_Change volume for item under mouse cursor (mousewheel).lua

--- Items Properties/mpl_Change volume for item under mouse cursor (mousewheel).lua
+++ Items Properties/mpl_Change volume for item under mouse cursor (mousewheel).lua
@@ -1,5 +1,5 @@
 -- @description Change volume for item under mouse cursor (mousewheel)
--- @version 1.04
+-- @version 1.05
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
@@ -8,7 +8,18 @@
   --NOT gfx NOT reaper
   
 
-  
+  for key in pairs(reaper) do _G[key]=reaper[key]  end 
+  ---------------------------------------------------
+  function VF_CheckReaperVrs(rvrs, showmsg) 
+    local vrs_num =  GetAppVersion()
+    vrs_num = tonumber(vrs_num:match('[%d%.]+'))
+    if rvrs > vrs_num then 
+      if showmsg then reaper.MB('Update REAPER to newer version '..'('..rvrs..' or newer)', '', 0) end
+      return
+     else
+      return true
+    end
+  end
   ------------------------------------------------------------------------------------------------------
   function VF_GetItemTakeUnderMouseCursor()
     local screen_x, screen_y = GetMousePosition()
