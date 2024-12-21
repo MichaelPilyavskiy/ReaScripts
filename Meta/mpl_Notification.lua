3c63eec3f6da9e548c6240a7d77f411b02f6f012
MPL <m.pilyavskiy@gmail.com>
2024-12-21 13:08:12 +0300

    21.12

--- Meta/mpl_Notification.lua
+++ Meta/mpl_Notification.lua
@@ -1,5 +1,5 @@
 -- @description Notification
--- @version 1.0
+-- @version 1.01
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=165672
 -- @about Script for showing custom notification
