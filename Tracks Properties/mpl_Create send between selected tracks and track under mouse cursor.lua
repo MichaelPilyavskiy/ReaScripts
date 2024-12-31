c8a4e4b6f962955863beba61f6103cf6d0f41edd
MPL <m.pilyavskiy@gmail.com>
2024-12-31 09:16:58 +0300

    Update mpl_Create send between selected tracks and track under mouse cursor.lua

--- Tracks Properties/mpl_Create send between selected tracks and track under mouse cursor.lua
+++ Tracks Properties/mpl_Create send between selected tracks and track under mouse cursor.lua
@@ -1,5 +1,5 @@
 -- @description Create send between selected tracks and track under mouse cursor
--- @version 1.20
+-- @version 1.21
 -- @author MPL
 -- @metapackage
 -- @provides
@@ -40,7 +40,7 @@
 --    [main] . > mpl_Send track under mouse cursor to selected tracks (channel 15-16 to 1-2).lua
 -- @website http://forum.cockos.com/showthread.php?t=188335  
 -- @changelog
---    # VF independent
+--    # replace missed data for default send flags by -2.0dB in postfader mode
 
   for key in pairs(reaper) do _G[key]=reaper[key]  end 
   ---------------------------------------------------
@@ -264,12 +264,12 @@
     
     -- Section was not found
     if not section_found then 
-      reaper.ShowConsoleMsg("Couldn't find section: " .. section .. "\n")
-      return false
+      --reaper.ShowConsoleMsg("Couldn't find section: " .. section .. "\n")
+      return-- false
     end
     if not key_found then 
-      if section_found and not key_found then reaper.ShowConsoleMsg("Couldn't find key: " .. key .. "\n") end
-    return false
+      --if section_found and not key_found then reaper.ShowConsoleMsg("Couldn't find key: " .. key .. "\n") end
+    return-- false
     end
   end
   ------------------------------------------------------------------------------------------------------
@@ -293,8 +293,9 @@
   end 
   ----------------------------------------------------------------------
   if VF_CheckReaperVrs(5.975,true)  then 
-    local defsendvol = VF_spk77_getinivalue( get_ini_file(), 'REAPER', 'defsendvol')
-    local defsendflag = VF_spk77_getinivalue( get_ini_file(), 'REAPER', 'defsendflag')
+    local defsendvol = VF_spk77_getinivalue( get_ini_file(), 'REAPER', 'defsendvol') or 0.79432823
+    local defsendflag = VF_spk77_getinivalue( get_ini_file(), 'REAPER', 'defsendflag') or 256
+    
     local source_type, MCH_mode, src_ch, dest_ch, script_title, custom_sendmode = Parsing_filename()
     local data_t = {source_type=source_type, MCH_mode=MCH_mode, src_ch=src_ch, dest_ch=dest_ch, script_title=script_title, defsendvol=defsendvol or 1, defsendflag=defsendflag or 256, custom_sendmode=custom_sendmode}
     main(data_t)
