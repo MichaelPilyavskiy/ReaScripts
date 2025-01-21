3df015318baba13232cd5f33af24bcfeef8d5740
MPL <m.pilyavskiy@gmail.com>
2025-01-21 22:10:56 +0300

    Update mpl_Check if multichannel item has mono source, convert to mono item if need .lua

--- Takes Properties/mpl_Check if multichannel item has mono source, convert to mono item if need .lua
+++ Takes Properties/mpl_Check if multichannel item has mono source, convert to mono item if need .lua
@@ -1,12 +1,9 @@
 -- @description Check if multichannel item has mono source, convert to mono item if need 
--- @version 1.02
+-- @version 1.03
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    # improve math a bit
---    # upper thresholds
---    # offset audio accessor position a bit, for some reason it doesn`t give correct results
---    + Pan mono take if source is panned mono
+--    # improve math
 
   for key in pairs(reaper) do _G[key]=reaper[key]  end 
   ---------------------------------------------------
@@ -20,6 +17,7 @@
       return true
     end
   end
+    function msg(s)  if not s then return end  if type(s) == 'boolean' then if s then s = 'true' else  s = 'false' end end ShowConsoleMsg(s..'\n') end 
   ---------------------------------------------------
   function main()
     for i = 1, CountMediaItems(0) do
@@ -27,19 +25,10 @@
       if IsMediaItemSelected(item) then
         local take = GetActiveTake(item) 
         if take then 
-          local ismono,real_ratiodiff_RMS = MonoCheck(item, take)
+          local ismono,outpan = MonoCheck(item, take)
           if ismono==true then  
-            local swap = -1
-            SetMediaItemTakeInfo_Value( take, 'I_CHANMODE' , 3) 
-            if real_ratiodiff_RMS then -- 1/2=50%L 1 = C 2= 50R
-              if real_ratiodiff_RMS > 1 then 
-                real_ratiodiff_RMS = 1/real_ratiodiff_RMS 
-                swap = 1
-              end
-              R =  real_ratiodiff_RMS
-              L =  1-real_ratiodiff_RMS
-              SetMediaItemTakeInfo_Value( take, 'D_PAN' , swap*(L-0.5)*2) 
-            end
+            SetMediaItemTakeInfo_Value( take, 'I_CHANMODE' , 2) 
+            if outpan then SetMediaItemTakeInfo_Value( take, 'D_PAN' ,outpan)  end
             UpdateItemInProject( item ) 
           end
         end
@@ -49,44 +38,55 @@
     ----------------------------------------------------------------------
   function WDL_DB2VAL(x) return math.exp((x)*0.11512925464970228420089957273422) end  --https://github.com/majek/wdl/blob/master/WDL/db2val.h  
   ----------------------------------------------------------------------
-  function MonoCheck(item, take)
-    local accessor = CreateTakeAudioAccessor( take )
-    local it_len = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
+  function MonoCheck_GetTestPoints(item, take, divisions)  
+    local accessor = CreateTakeAudioAccessor( take ) 
     local src  = reaper.GetMediaItemTake_Source( take )
     local SR =  reaper.GetMediaSourceSampleRate( src )
     local test_chan = reaper.GetMediaSourceNumChannels( src ) 
-    local diff_com = 0 
-    local test_dist = it_len / 10
-    local threshold_lin = WDL_DB2VAL(-80)--0.001 -- -80db 
-    local  real_ratiodiff_t = {}
-    local  real_ratiodiff_RMS = 0
-    --local threshold_dB = -80
+    local it_len = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
+    local test_dist = it_len / (divisions or 10)
+    local threshold_lin = WDL_DB2VAL(-60)--0.0001 -- -80db 
+    local test_points = {}
     for pos = 0.01, it_len-test_dist, test_dist do 
       local samplebuffer = new_array(test_chan);
       GetAudioAccessorSamples( accessor, SR, test_chan, pos, 1, samplebuffer )
       for i = 2, test_chan do
-        local diff =math.abs(samplebuffer[i]-samplebuffer[1])
-        if samplebuffer[1] ~= 0 then 
-          local ratio  = samplebuffer[i]/samplebuffer[1] 
-          real_ratiodiff_t[#real_ratiodiff_t+1] =ratio
-          real_ratiodiff_RMS=real_ratiodiff_RMS+ratio
-        end
-        if diff > threshold_lin then diff_com = diff_com + 1 end
+        local primary = samplebuffer[1] 
+        local secondary = samplebuffer[i]
+        if (math.abs(primary) >= threshold_lin or  math.abs(secondary) >= threshold_lin) then
+          local diff = (secondary - primary) / (secondary + primary)
+          test_points[#test_points+1] = {
+              secondary = secondary ,
+              primary = primary,
+              diff=diff
+            } 
+        end 
       end
       samplebuffer.clear()
-    end
+    end 
     DestroyAudioAccessor( accessor ) 
+    return test_points
+  end
+  ----------------------------------------------------------------------
+  function MonoCheck(item, take)
+    test_points = MonoCheck_GetTestPoints(item, take)
     
-    -- no difference
-    if diff_com == 0 then return true end
+    -- try again if not enough points
+    if #test_points< 2 then test_points = MonoCheck_GetTestPoints(item, take, 20) end
     
-    -- closer value as channels ratio
-    local real_ratiodiff_RMS = real_ratiodiff_RMS  /   #real_ratiodiff_t
-    local is_pannedmono = true
-    for i = 1, #real_ratiodiff_t do
-      if math.abs(real_ratiodiff_t[i] - real_ratiodiff_RMS) > 0.01 then return end
-    end
-    if is_pannedmono == true then return true,real_ratiodiff_RMS end
+    -- all file is too quiet
+    if #test_points< 2 then return end
+    
+    -- get rms difference
+    differenceRMS = 0
+    for i = 1, #test_points do differenceRMS = differenceRMS + test_points[i].diff end
+    differenceRMS =  differenceRMS / #test_points
+    
+    -- make sure difference is same along all segments of item
+    local stablediff_lin = WDL_DB2VAL(-60)
+    for i = 1, #test_points do if math.abs(differenceRMS - test_points[i].diff) > stablediff_lin then return end end
+    
+    return true, differenceRMS
   end   
   ----------------------------------------------------------------------
   if VF_CheckReaperVrs(5.9,true) then 
