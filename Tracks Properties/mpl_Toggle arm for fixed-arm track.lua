d8103daa0f3e6c23c79ee50620418970cc338387
MPL <m.pilyavskiy@gmail.com>
2025-10-18 09:42:41 +0300

    Update mpl_Toggle arm for fixed-arm track.lua

--- Tracks Properties/mpl_Toggle arm for fixed-arm track.lua
+++ Tracks Properties/mpl_Toggle arm for fixed-arm track.lua
@@ -1,9 +1,9 @@
 -- @description Toggle arm for fixed-arm track
--- @version 1.01
+-- @version 1.02
 -- @author MPL
 -- @website http://forum.cockos.com/showthread.php?t=188335
 -- @changelog
---    + refresh toolbar
+--    # fix exit if selecled track not exiusts
 
   for key in pairs(reaper) do _G[key]=reaper[key]  end 
   ---------------------------------------------------
@@ -17,6 +17,7 @@
       return true
     end
   end
+    ---------------------------------------------------------------------
   function VF_GetTrackByGUID(giv_guid, reaproj)
     if not (giv_guid and giv_guid:gsub('%p+','')) then return end
     for i = 1, CountTracks(reaproj or -1) do
@@ -28,9 +29,7 @@
   end
   ---------------------------------------------------------------------  
   function main()  
-    local tr = GetSelectedTrack(-1,0)
-    if not tr then return end
-    ret, giv_guid = GetProjExtState( -1, 'MPL_FIXARM', 'TRGUID')
+    local ret, giv_guid = GetProjExtState( -1, 'MPL_FIXARM', 'TRGUID')
     if ret and giv_guid ~= '' then 
       tr = VF_GetTrackByGUID(giv_guid)
       if tr then 
@@ -40,7 +39,7 @@
       end
     end
   end
-    ----------------------------------------------------------------------
+  ----------------------------------------------------------------------
   function SetButtonState( set )
     local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
     reaper.SetToggleCommandState( sec, cmd, set or 0 )
